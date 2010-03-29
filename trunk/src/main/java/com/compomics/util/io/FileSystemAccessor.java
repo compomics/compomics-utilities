5eab4641e91c114580518ada20f27dacc468acfa
kennyhelsens <kennyhelsens@f644b618-123a-11df-8689-7786f20063ad>
2010-03-29 16:35:33 +0000

    - Introduced the java.logging platform for compomics tools.

--- trunk/src/main/java/com/compomics/util/io/FileSystemAccessor.java
+++ trunk/src/main/java/com/compomics/util/io/FileSystemAccessor.java
@@ -43,4 +43,20 @@ public class FileSystemAccessor {
 
         return lFile;
     }
+
+    /**
+     * Returns the absolute path of the underlying file system of the given class.
+     * @param aClassname The classname of which the parent folder needs to be returned (e.g.: "FileSystemAccessor.class")
+     * @return The full path to the parent classname. (e.g.: /home/user/java/compomics/utilities/)
+     */
+    public static String getPathOfClass(String aClassname){
+        String path;
+        String lFileSeparator = System.getProperties().get("file.separator").toString();
+        // PropertiesManager serves as a class of the Utilities libary to find the resources
+        path = PropertiesManager.getInstance().getClass().getProtectionDomain().getCodeSource().getLocation().toString();
+        path = path.substring(5, path.lastIndexOf(lFileSeparator) + 1);
+        path = path.replace("%20", " ");
+
+        return path;
+    }
 }

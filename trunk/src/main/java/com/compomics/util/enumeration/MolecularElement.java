146f31a50fcd9a1daf8557b4f1548a70f019ae72
niklaas.colaert@ugent.be <niklaas.colaert@ugent.be@f644b618-123a-11df-8689-7786f20063ad>
2010-08-17 06:36:59 +0000

    Added the isotopic distribution calculator

---
+++ trunk/src/main/java/com/compomics/util/enumeration/MolecularElement.java
@@ -0,0 +1,18 @@
+package com.compomics.util.enumeration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Niklaas
+ * Date: 16-Aug-2010
+ * Time: 08:28:22
+ */
+public enum MolecularElement {
+
+    H,C,N,O,S,HDeut,C13,N15,O18;
+
+    /**
+     * Private constructor to initialize variables for each type.
+     */
+    private MolecularElement() {
+    }
+}
---
+++ trunk/src/main/java/com/compomics/util/general/IsotopicDistributionSpectrum.java
@@ -0,0 +1,104 @@
+package com.compomics.util.general;
+
+import com.compomics.util.interfaces.SpectrumFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Niklaas
+ * Date: 16-Aug-2010
+ * Time: 14:36:23
+ */
+
+public class IsotopicDistributionSpectrum implements SpectrumFile {
+    private int iCharge;
+    private HashMap iPeaks = new HashMap();
+    private double iPrecursorMass;
+
+    public int getCharge() {
+        return iCharge;
+    }
+
+    public void setCharge(int aCharge) {
+        iCharge = aCharge;
+    }
+
+    public String getFilename() {
+        return "Isotopic distribution";
+    }
+
+    public void setFilename(String aFilename) {
+
+    }
+
+    public HashMap getPeaks() {
+        return iPeaks;
+    }
+
+    public void setPeaks(HashMap aPeaks) {
+        iPeaks = aPeaks;
+    }
+
+    public double getPrecursorMZ() {
+        return iPrecursorMass;
+    }
+
+    public void setPrecursorMZ(double aPrecursorMZ) {
+        this.iPrecursorMass = aPrecursorMZ;
+    }
+
+    public double getIntensity() {
+        return 0;
+    }
+
+    public void setIntensity(double aIntensity) {
+
+    }
+
+    public double getTotalIntensity() {
+        Iterator iter = this.iPeaks.values().iterator();
+        double totalIntensity = 0.0;
+        while (iter.hasNext()) {
+            totalIntensity += (Double) iter.next();
+        }
+        return round(totalIntensity);
+    }
+
+
+    public double getHighestIntensity() {
+        Iterator iter = this.iPeaks.values().iterator();
+        double highestIntensity = -1.0;
+        while (iter.hasNext()) {
+            double temp = (Double) iter.next();
+            if (temp > highestIntensity) {
+                highestIntensity = temp;
+            }
+        }
+        return round(highestIntensity);
+    }
+
+    public void writeToStream(OutputStream aOut) throws IOException {
+
+    }
+
+    public void writeToFile(File aParentDir) throws IOException {
+
+    }
+
+    public int compareTo(Object o) {
+        return 0;
+    }
+
+    private double round(final double aTotalIntensity) {
+        BigDecimal bd = new BigDecimal(aTotalIntensity).setScale(2, RoundingMode.UP);
+        return bd.doubleValue();
+    }
+
+}

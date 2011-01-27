12d809ed9be60003416114804a7edeb98e65b6e7
mvaudel <mvaudel@f644b618-123a-11df-8689-7786f20063ad>
2011-01-27 17:06:30 +0000

    javadoc

--- trunk/src/main/java/com/compomics/util/experiment/identification/FastaHeaderParser.java
+++ trunk/src/main/java/com/compomics/util/experiment/identification/FastaHeaderParser.java
@@ -7,9 +7,20 @@ package com.compomics.util.experiment.identification;
  */
 public class FastaHeaderParser {
 
+    /**
+     * String found before the protein accession
+     */
     private String beforeAccession;
+    /**
+     * String found after the protein accession
+     */
     private String afterAccession;
 
+    /**
+     * Constructor for a fasta header parser
+     * @param beforeAccession   String found before the protein accession
+     * @param afterAccession    String found after the protein accession
+     */
     public FastaHeaderParser(String beforeAccession, String afterAccession) {
         if (beforeAccession.equals("")) {
             beforeAccession = ">";
@@ -18,12 +29,22 @@ public class FastaHeaderParser {
         this.afterAccession = afterAccession;
     }
 
+    /**
+     * Returns the protein accession from a fasta header
+     * @param header the fasta header to parse
+     * @return  The corresponding accession
+     */
     public String getProteinAccession(String header) {
         int start = header.indexOf(beforeAccession);
         int end = header.indexOf(afterAccession, ++start);
         return header.substring(start, end);
     }
 
+    /**
+     * Returns the Protein description from a fasta header
+     * @param header the fasta header to parse
+     * @return The corresponding description
+     */
     public String getProteinDescription(String header) {
         int start = header.indexOf(beforeAccession);
         int end = header.indexOf(afterAccession, ++start);
--- trunk/src/main/java/com/compomics/util/experiment/identification/SequenceDataBase.java
+++ trunk/src/main/java/com/compomics/util/experiment/identification/SequenceDataBase.java
@@ -17,21 +17,50 @@ import java.util.Set;
  */
 public class SequenceDataBase extends ExperimentObject {
 
+    /**
+     * Flag for a decoy protein
+     */
     public static final String decoyFlag = "REV";  // This ough not to be hard coded
+    /**
+     * Name of the database
+     */
     private String name;
+    /**
+     * Version of the database
+     */
     private String version;
+    /**
+     * Protein map with proteins indexed by their key.
+     */
     private HashMap<String, Protein> proteinMap = new HashMap<String, Protein>();
+    /**
+     * Number of targeted sequences loaded
+     */
     private int nTargetSequences = 0;
 
+    /**
+     * Constructor for a sequence database
+     * @param name      Name of the database
+     * @param version   Version of the database
+     */
     public SequenceDataBase(String name, String version) {
         this.name = name;
         this.version = version;
     }
 
+    /**
+     * Returns a protein indexed by its key
+     * @param proteinKey protein key
+     * @return the corresponding key
+     */
     public Protein getProtein(String proteinKey) {
         return proteinMap.get(proteinKey);
     }
 
+    /**
+     * Adds a protein to the map
+     * @param protein the new protein
+     */
     public void addProtein(Protein protein) {
         proteinMap.put(protein.getProteinKey(), protein);
         if (!protein.isDecoy()) {
@@ -39,14 +68,30 @@ public class SequenceDataBase extends ExperimentObject {
         }
     }
 
+    /**
+     * returns the number of target hits loaded
+     * @return the number of target hits loaded
+     */
     public int getNumberOfTargetSequences() {
         return nTargetSequences;
     }
 
+    /**
+     * Returns the list of all protein imported
+     * @return list of all protein imported
+     */
     public Set<String> getProteinList() {
         return proteinMap.keySet();
     }
 
+    /**
+     * Imports a sequence database from a fasta file
+     *
+     * @param fastaHeaderParser         Parser for a fasta header
+     * @param fastaFile                 The fasta file to import
+     * @throws FileNotFoundException    Error thrown when the fasta file is not found
+     * @throws IOException              Error thrown whenever an error is encountered while parsing the file
+     */
     public void importDataBase(FastaHeaderParser fastaHeaderParser, File fastaFile) throws FileNotFoundException, IOException {
         FileReader f = new FileReader(fastaFile);
         BufferedReader b = new BufferedReader(f);

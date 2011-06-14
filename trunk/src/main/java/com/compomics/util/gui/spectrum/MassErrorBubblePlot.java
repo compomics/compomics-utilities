6fc28bd7a43076b6e5ae4abdce183e7a7e590b2d
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2011-06-14 18:42:36 +0000

    Added code for extracting the chart panels from the mass error plots. Moved the project to version 3.1.22.

--- trunk/src/main/java/com/compomics/util/gui/spectrum/MassErrorBubblePlot.java
+++ trunk/src/main/java/com/compomics/util/gui/spectrum/MassErrorBubblePlot.java
@@ -75,6 +75,10 @@ public class MassErrorBubblePlot extends JPanel {
      * The list of currently used ions.
      */
     private ArrayList<IonMatch> currentlyUsedIonMatches;
+    /**
+     * The chart panel.
+     */
+    private ChartPanel chartPanel;
 
     /**
      * Creates a new MassErrorBubblePlot.
@@ -271,7 +275,7 @@ public class MassErrorBubblePlot extends JPanel {
         chart.getPlot().setBackgroundPaint(Color.WHITE);
         chart.setBackgroundPaint(Color.WHITE);
 
-        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel = new ChartPanel(chart);
         chartPanel.setBackground(Color.WHITE);
 
         this.add(chartPanel);
@@ -458,5 +462,14 @@ public class MassErrorBubblePlot extends JPanel {
         }
 
         return dataset;
+    }
+    
+    /**
+     * Returns the chart panel.
+     * 
+     * @return the chart panel
+     */
+    public ChartPanel getChartPanel () {
+        return chartPanel;
     }
 }

20ce0e28e5515a11fe604ac07f84b795984c01f5
mvaudel <mvaudel@f644b618-123a-11df-8689-7786f20063ad>
2012-01-30 14:11:13 +0000

    Made the unknown protein public

--- trunk/src/main/java/com/compomics/util/experiment/biology/PTMFactory.java
+++ trunk/src/main/java/com/compomics/util/experiment/biology/PTMFactory.java
@@ -49,7 +49,7 @@ public class PTMFactory implements Serializable {
     /**
      * unknown modification to be returned when the modification is not found
      */
-    private static final PTM unknownPTM = new PTM(PTM.MODAA, "unknown", 0, new ArrayList<String>());
+    public static final PTM unknownPTM = new PTM(PTM.MODAA, "unknown", 0, new ArrayList<String>());
 
     /**
      * Constructor for the factory
@@ -248,9 +248,6 @@ public class PTMFactory implements Serializable {
      * @return the candidate modification, null if none is found
      */
     public PTM getPTM(double mass, String location, String sequence) {
-        if (location.contains("K")) {
-            int debug = 1;
-        }
         for (PTM currentPTM : ptmMap.values()) {
             if (currentPTM.getType() == PTM.MODAA
                     || currentPTM.getType() == PTM.MODCAA

01b00e4e4fbe7f54de7d00ddba000a415e900bf9
mvaudel <mvaudel@f644b618-123a-11df-8689-7786f20063ad>
2012-03-30 11:31:12 +0000

    commented out test code

--- src/test/java/com/compomics/util/test/experiment/fastaTest.java
+++ src/test/java/com/compomics/util/test/experiment/fastaTest.java
@@ -39,100 +39,105 @@ public class fastaTest extends TestCase {
             e.printStackTrace();
         }
     }
-
-    public void testJulia() {
-        try {
-            
-            String fileName = "YPD_N-terminal_UP";
-            
-            ArrayList<String> peptides = new ArrayList<String>();
-            HashMap<String, String> remarks = new HashMap<String, String>();
-            File peptidesFile = new File("src/test/resources/experiment/" + fileName + ".txt");
-            FileReader f = new FileReader(peptidesFile);
-            BufferedReader b = new BufferedReader(f);
-            String line;
-            while ((line = b.readLine()) != null) {
-                String[] input = line.split("\t");
-                String peptide = input[0].trim();
-                if (!peptide.equals("")) {
-                    peptides.add(peptide);
-                    remarks.put(peptide, input[1].trim());
-                }
-            }
-            b.close();
-            f.close();
-
-            File fastaFile = new File("src/test/resources/experiment/merged_SGD20110203_UPS_12_adapted_20120201.fasta");
-            sequenceFactory.loadFastaFile(fastaFile);
-            String currentProteinSequence;
-            char aa;
-            HashMap<String, HashMap<String, HashMap<Integer, String>>> aaBefore = new HashMap<String, HashMap<String, HashMap<Integer, String>>>();
-            HashMap<String, HashMap<String, HashMap<Integer, String>>> aaAfter = new HashMap<String, HashMap<String, HashMap<Integer, String>>>();
-            for (String accession : sequenceFactory.getAccessions()) {
-                currentProteinSequence = sequenceFactory.getProtein(accession).getSequence();
-                for (String peptide : peptides) {
-                    String tempSequence = currentProteinSequence;
-                    while (tempSequence.lastIndexOf(peptide) >= 0) {
-                    String subsequence = "";
-                        int startIndex = tempSequence.lastIndexOf(peptide) - 5;
-                        for (int index = startIndex; index < startIndex + 5; index++) {
-                            if (index >= 0 && index < currentProteinSequence.length()) {
-                                aa = currentProteinSequence.charAt(index);
-                                if (aa != '*') {
-                                    subsequence += aa;
-                                }
-                            }
-                        }
-                        if (!aaBefore.containsKey(accession)) {
-                            aaBefore.put(accession, new HashMap<String, HashMap<Integer, String>>());
-                        }
-                        if (!aaBefore.get(accession).containsKey(peptide)) {
-                            aaBefore.get(accession).put(peptide, new HashMap<Integer, String>());
-                        }
-                        aaBefore.get(accession).get(peptide).put(startIndex + 6, subsequence);
-                        subsequence = "";
-                        for (int index = startIndex + peptide.length() + 5; index < startIndex + peptide.length() + 10; index++) {
-                            if (index >= 0 && index < currentProteinSequence.length()) {
-                                aa = currentProteinSequence.charAt(index);
-                                if (aa != '*') {
-                                    subsequence += aa;
-                                }
-                            }
-                        }
-                        if (!aaAfter.containsKey(accession)) {
-                            aaAfter.put(accession, new HashMap<String, HashMap<Integer, String>>());
-                        }
-                        if (!aaAfter.get(accession).containsKey(peptide)) {
-                            aaAfter.get(accession).put(peptide, new HashMap<Integer, String>());
-                        }
-                        aaAfter.get(accession).get(peptide).put(startIndex + 6, subsequence);
-                        tempSequence = currentProteinSequence.substring(0, startIndex + 6);
-                    }
-                }
-            }
-
-            System.out.println("writing results");
-            File outputFile1 = new File("src/test/resources/experiment/" + fileName + " subsequences.txt");
-            FileWriter fw1 = new FileWriter(outputFile1);
-            BufferedWriter bw1 = new BufferedWriter(fw1);
-            bw1.write("Accession\tPeptide Start\tAA before\tPeptide\tAA after\tComment\n");
-            ArrayList<Integer> indexes;
-            for (String accession : aaBefore.keySet()) {
-                for (String peptide : aaBefore.get(accession).keySet()) {
-                    indexes = new ArrayList<Integer>(aaBefore.get(accession).get(peptide).keySet());
-                    Collections.sort(indexes);
-                    for (int index : indexes) {
-                        bw1.write(accession + "\t" + index + "\t" + aaBefore.get(accession).get(peptide).get(index) + "\t" + peptide + "\t" + aaAfter.get(accession).get(peptide).get(index) + "\t" + remarks.get(peptide) + "\n");
-                    }
-                }
-            }
-            bw1.close();
-            fw1.close();
-
-        } catch (Exception e) {
-            int debug = 1;
-        }
-    }
+    
+    
+    
+    
+//    }
+//
+//    public void testJulia() {
+//        try {
+//            
+//            String fileName = "YPD_N-terminal_UP";
+//            
+//            ArrayList<String> peptides = new ArrayList<String>();
+//            HashMap<String, String> remarks = new HashMap<String, String>();
+//            File peptidesFile = new File("src/test/resources/experiment/" + fileName + ".txt");
+//            FileReader f = new FileReader(peptidesFile);
+//            BufferedReader b = new BufferedReader(f);
+//            String line;
+//            while ((line = b.readLine()) != null) {
+//                String[] input = line.split("\t");
+//                String peptide = input[0].trim();
+//                if (!peptide.equals("")) {
+//                    peptides.add(peptide);
+//                    remarks.put(peptide, input[1].trim());
+//                }
+//            }
+//            b.close();
+//            f.close();
+//
+//            File fastaFile = new File("src/test/resources/experiment/merged_SGD20110203_UPS_12_adapted_20120201.fasta");
+//            sequenceFactory.loadFastaFile(fastaFile);
+//            String currentProteinSequence;
+//            char aa;
+//            HashMap<String, HashMap<String, HashMap<Integer, String>>> aaBefore = new HashMap<String, HashMap<String, HashMap<Integer, String>>>();
+//            HashMap<String, HashMap<String, HashMap<Integer, String>>> aaAfter = new HashMap<String, HashMap<String, HashMap<Integer, String>>>();
+//            for (String accession : sequenceFactory.getAccessions()) {
+//                currentProteinSequence = sequenceFactory.getProtein(accession).getSequence();
+//                for (String peptide : peptides) {
+//                    String tempSequence = currentProteinSequence;
+//                    while (tempSequence.lastIndexOf(peptide) >= 0) {
+//                    String subsequence = "";
+//                        int startIndex = tempSequence.lastIndexOf(peptide) - 5;
+//                        for (int index = startIndex; index < startIndex + 5; index++) {
+//                            if (index >= 0 && index < currentProteinSequence.length()) {
+//                                aa = currentProteinSequence.charAt(index);
+//                                if (aa != '*') {
+//                                    subsequence += aa;
+//                                }
+//                            }
+//                        }
+//                        if (!aaBefore.containsKey(accession)) {
+//                            aaBefore.put(accession, new HashMap<String, HashMap<Integer, String>>());
+//                        }
+//                        if (!aaBefore.get(accession).containsKey(peptide)) {
+//                            aaBefore.get(accession).put(peptide, new HashMap<Integer, String>());
+//                        }
+//                        aaBefore.get(accession).get(peptide).put(startIndex + 6, subsequence);
+//                        subsequence = "";
+//                        for (int index = startIndex + peptide.length() + 5; index < startIndex + peptide.length() + 10; index++) {
+//                            if (index >= 0 && index < currentProteinSequence.length()) {
+//                                aa = currentProteinSequence.charAt(index);
+//                                if (aa != '*') {
+//                                    subsequence += aa;
+//                                }
+//                            }
+//                        }
+//                        if (!aaAfter.containsKey(accession)) {
+//                            aaAfter.put(accession, new HashMap<String, HashMap<Integer, String>>());
+//                        }
+//                        if (!aaAfter.get(accession).containsKey(peptide)) {
+//                            aaAfter.get(accession).put(peptide, new HashMap<Integer, String>());
+//                        }
+//                        aaAfter.get(accession).get(peptide).put(startIndex + 6, subsequence);
+//                        tempSequence = currentProteinSequence.substring(0, startIndex + 6);
+//                    }
+//                }
+//            }
+//
+//            System.out.println("writing results");
+//            File outputFile1 = new File("src/test/resources/experiment/" + fileName + " subsequences.txt");
+//            FileWriter fw1 = new FileWriter(outputFile1);
+//            BufferedWriter bw1 = new BufferedWriter(fw1);
+//            bw1.write("Accession\tPeptide Start\tAA before\tPeptide\tAA after\tComment\n");
+//            ArrayList<Integer> indexes;
+//            for (String accession : aaBefore.keySet()) {
+//                for (String peptide : aaBefore.get(accession).keySet()) {
+//                    indexes = new ArrayList<Integer>(aaBefore.get(accession).get(peptide).keySet());
+//                    Collections.sort(indexes);
+//                    for (int index : indexes) {
+//                        bw1.write(accession + "\t" + index + "\t" + aaBefore.get(accession).get(peptide).get(index) + "\t" + peptide + "\t" + aaAfter.get(accession).get(peptide).get(index) + "\t" + remarks.get(peptide) + "\n");
+//                    }
+//                }
+//            }
+//            bw1.close();
+//            fw1.close();
+//
+//        } catch (Exception e) {
+//            int debug = 1;
+//        }
+//    }
     /**
      * public void testComparison() { try { System.out.println("Importing
      * archaea peptides"); File fastaFile = new

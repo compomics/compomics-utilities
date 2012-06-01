ccb6f39373e3f7d108a5df0e237af08a525000a8
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2012-06-01 16:38:25 +0000

    Added support for the amino acid Pyrrolysine.

---
+++ src/main/java/com/compomics/util/experiment/biology/aminoacids/Pyrrolysine.java
@@ -0,0 +1,19 @@
+package com.compomics.util.experiment.biology.aminoacids;
+
+import com.compomics.util.experiment.biology.AminoAcid;
+
+/**
+ * Pyrrolysine
+ *
+ * @author Harald Barsnes
+ */
+public class Pyrrolysine extends AminoAcid {
+
+    public Pyrrolysine() {
+        singleLetterCode = "O";
+        threeLetterCode = "Pyl";
+        name = "Pyrrolysine";
+        averageMass = 255.3134;
+        monoisotopicMass = 255.158295;
+    }
+}

20883f105a336f55f919082630687b5c93dfbe81
mvaudel <mvaudel@f644b618-123a-11df-8689-7786f20063ad>
2012-10-10 21:01:20 +0000

    improved the handling of exceptions of the serialization methods

--- trunk/src/main/java/com/compomics/util/io/SerializationUtils.java
+++ trunk/src/main/java/com/compomics/util/io/SerializationUtils.java
@@ -7,44 +7,92 @@ package com.compomics.util.io;
 import java.io.*;
 
 /**
- * This class implements convenience methods for serialization and deserialization
+ * This class implements convenience methods for serialization and
+ * deserialization
  *
  * @author Marc
  */
 public class SerializationUtils {
-    
+
     /**
      * Writes an object to the destination file
+     *
      * @param object the object
      * @param destinationFile the destination file
-     * @throws IOException exception thrown whenever an error occurred while writing the file
+     * @throws IOException exception thrown whenever an error occurred while
+     * writing the file
      */
     public static void writeObject(Object object, File destinationFile) throws IOException {
-        FileOutputStream fos = new FileOutputStream(destinationFile);
-        BufferedOutputStream bos = new BufferedOutputStream(fos);
-        ObjectOutputStream oos = new ObjectOutputStream(bos);
-        oos.writeObject(object);
-        oos.close();
-        bos.close();
-        fos.close();
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        ObjectOutputStream oos = null;
+        try {
+            fos = new FileOutputStream(destinationFile);
+            bos = new BufferedOutputStream(fos);
+            oos = new ObjectOutputStream(bos);
+            oos.writeObject(object);
+            oos.close();
+            bos.close();
+            fos.close();
+        } catch (IOException e) {
+            if (oos != null) {
+                oos.close();
+            }
+            if (bos != null) {
+                bos.close();
+            }
+            if (fos != null) {
+                fos.close();
+            }
+            throw e;
+        }
     }
-    
+
     /**
      * Reads an object from a serialized file
+     *
      * @param serializedFile the serialized file
      * @return the object
-     * @throws IOException exception thrown whenever an error occurred while reading the file
-     * @throws ClassNotFoundException exception thrown whenever an unknown class is found
+     * @throws IOException exception thrown whenever an error occurred while
+     * reading the file
+     * @throws ClassNotFoundException exception thrown whenever an unknown class
+     * is found
      */
     public static Object readObject(File serializedFile) throws IOException, ClassNotFoundException {
-        FileInputStream fis = new FileInputStream(serializedFile);
-        BufferedInputStream bis = new BufferedInputStream(fis);
-        ObjectInputStream in = new ObjectInputStream(bis);
-        Object object = in.readObject();
-        in.close();
-        fis.close();
-        bis.close();
-        return object;
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        ObjectInputStream in = null;
+        try {
+            fis = new FileInputStream(serializedFile);
+            bis = new BufferedInputStream(fis);
+            in = new ObjectInputStream(bis);
+            Object object = in.readObject();
+            in.close();
+            fis.close();
+            bis.close();
+            return object;
+        } catch (IOException e) {
+            if (in != null) {
+                in.close();
+            }
+            if (fis != null) {
+                fis.close();
+            }
+            if (bis != null) {
+                bis.close();
+            }
+            throw e;
+        } catch (ClassNotFoundException e) {
+            if (in != null) {
+                in.close();
+            }
+            if (fis != null) {
+                fis.close();
+            }
+            if (bis != null) {
+                bis.close();
+            }
+            throw e;
+        }
     }
-    
 }

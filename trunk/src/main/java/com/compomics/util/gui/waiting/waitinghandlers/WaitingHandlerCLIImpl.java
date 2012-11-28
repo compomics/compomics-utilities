9d1fee0c412e7a195c6434146304c106cfa3fd79
mvaudel <mvaudel@f644b618-123a-11df-8689-7786f20063ad>
2012-11-28 18:19:23 +0000

    added generic methods to the waiting handlers and the annotation preferences for command line compatibility

--- trunk/src/main/java/com/compomics/util/gui/waiting/waitinghandlers/WaitingHandlerCLIImpl.java
+++ trunk/src/main/java/com/compomics/util/gui/waiting/waitinghandlers/WaitingHandlerCLIImpl.java
@@ -15,133 +15,69 @@ import java.util.Date;
 public class WaitingHandlerCLIImpl implements WaitingHandler {
 
     /**
-     * Make use of the System Printstreams to log the messages.
+     * A boolean indicating whether the run was canceled
      */
-    private PrintStream err = System.err;
-    private PrintStream out = System.out;
-    /**
-     * The running value of the progress monitor.
-     */
-    private int iRunningProgressValue = 0;
-    private int iRunningProgressValueSecondary = 0;
-    /** 
-     * The upper value of the progress monitor.
-     */
-    private int iMaxProgressValue = 1;
-    private int iMaxProgressValueSecondary = 1;
-    private boolean boolSecondaryIntermediate = false;
-    private boolean boolFinished = false;
     private boolean boolCanceled = false;
+    /**
+     * The report to append
+     */
     protected String iReport = "";
 
-    /**
-     * Set the maximum value of the progress bar.
-     *
-     * @param maxProgressValue the max value
-     */
     @Override
     public void setMaxProgressValue(int maxProgressValue) {
-        iMaxProgressValue = maxProgressValue;
+        // not used in command line
     }
 
-    /**
-     * Increase the progress bar value by one "counter".
-     */
     @Override
     public void increaseProgressValue() {
-        iRunningProgressValue++;
+        // not used in command line
     }
 
-    /**
-     * Increase the progress bar value by the given amount.
-     *
-     * @param amount the amount to increase the value by
-     */
     @Override
     public void increaseProgressValue(int amount) {
-        iRunningProgressValue = iRunningProgressValue + amount;
+        // not used in command line
     }
 
-    /**
-     * Set the maximum value of the secondary progress bar. And resets the value
-     * to 0.
-     *
-     * @param maxProgressValue the max value
-     */
     @Override
     public void setMaxSecondaryProgressValue(int maxProgressValue) {
-        iMaxProgressValueSecondary = maxProgressValue;
-        resetSecondaryProgressBar();
-
+        // not used in command line
     }
 
-    /**
-     * Reset the secondary progress bar value to 0.
-     */
     @Override
     public void resetSecondaryProgressBar() {
-        iRunningProgressValueSecondary = 0;
+        // not used in command line
     }
 
-    /**
-     * Increase the secondary progress bar value by one "counter".
-     */
     @Override
     public void increaseSecondaryProgressValue() {
-        iRunningProgressValueSecondary++;
+        // not used in command line
     }
 
-    /**
-     * Sets the secondary progress bar to the given value.
-     *
-     * @param value the progress value
-     */
     @Override
     public void setSecondaryProgressValue(int value) {
-        iRunningProgressValue = value;
+        // not used in command line
     }
 
-    /**
-     * Increase the secondary progress bar value by the given amount.
-     *
-     * @param amount the amount to increase the value by
-     */
     @Override
     public void increaseSecondaryProgressValue(int amount) {
-        iRunningProgressValueSecondary = iRunningProgressValueSecondary + amount;
+        // not used in command line
     }
 
-    /**
-     * Sets the secondary progress bar to intermediate or not.
-     *
-     * @param intermediate if true, set to intermediate
-     */
     @Override
     public void setSecondaryProgressDialogIndeterminate(boolean intermediate) {
-        boolSecondaryIntermediate = intermediate;
+        // not used in command line
     }
 
-    /**
-     * Set the analysis as finished.
-     */
     @Override
     public void setRunFinished() {
-        boolFinished = true;
+        // not used in command line
     }
 
-    /**
-     * Set the analysis as canceled.
-     */
     @Override
     public void setRunCanceled() {
         boolCanceled = true;
     }
 
-    /**
-     * Append text to the report.
-     *
-     * @param report the text to append
-     */
     @Override
     public void appendReport(String report, boolean includeDate, boolean addNewLine) {
         
@@ -157,32 +93,21 @@ public class WaitingHandlerCLIImpl implements WaitingHandler {
         }
 
         iReport = iReport + tempReport;
-        out.append(tempReport);
+        System.out.append(tempReport);
     }
 
-    /**
-     * Append two tabs to the report. No new line.
-     */
     @Override
     public void appendReportNewLineNoDate() {
         iReport = iReport + System.getProperty("line.separator");
-        out.append(System.getProperty("line.separator"));
+        System.out.append(System.getProperty("line.separator"));
     }
 
-    /**
-     * Append a new line to the report.
-     */
     @Override
     public void appendReportEndLine() {
         iReport = iReport + System.getProperty("line.separator");
-        out.append(System.getProperty("line.separator"));
+        System.out.append(System.getProperty("line.separator"));
     }
 
-    /**
-     * Returns true if the run is canceled.
-     *
-     * @return true if the run is canceled
-     */
     @Override
     public boolean isRunCanceled() {
         return boolCanceled;
@@ -195,55 +120,35 @@ public class WaitingHandlerCLIImpl implements WaitingHandler {
      */
     @Override
     public JProgressBar getSecondaryProgressBar() {
-        return null;
+        throw new UnsupportedOperationException("This waiting handler has no progress bar.");
     }
 
-    /**
-     * Display a given message to the user separatly from the main output. For
-     * example a warning or error message. Usually in a separate dialog if a
-     * grahical waiting handler is used.
-     *
-     * @param message the message to display
-     * @param title the title of the message
-     * @param messageType the message type in the, e.g.,
-     * JOptionPane.INFORMATION_MESSAGE
-     */
     @Override
     public void displayMessage(String message, String title, int messageType) {
-        out.print(title);
-        out.print(System.getProperty("line.separator"));
-        out.print(message);
-        out.print(System.getProperty("line.separator"));
+        System.out.print(tab + message);
+        System.out.print(System.getProperty("line.separator"));
+        System.out.print(tab + title);
+        System.out.print(System.getProperty("line.separator"));
     }
 
-    /**
-     * Display a given html containing message to the user separatly from the
-     * main output. For example a warning or error message. Usually in a
-     * separate dialog if a grahical waiting handler is used. The html links
-     * should be clickable.
-     *
-     * @param messagePane
-     * @param title
-     * @param messageType
-     */
     @Override
     public void displayHtmlMessage(JEditorPane messagePane, String title, int messageType) {
         displayMessage(messagePane.getText(), title, messageType);
     }
 
-    /**
-     * Sets the text describing what is currently waited for.
-     *
-     * @param text a text describing what is currently waited for
-     */
     @Override
     public void setWaitingText(String text) {
-        displayMessage("WaitingMessage", text, 1);
+        displayMessage("Waiting Message:", text, 1);
 
     }
 
     @Override
     public JProgressBar getPrimaryProgressBar() {
         throw new UnsupportedOperationException("This waiting handler has no progress bar.");
+    }
+
+    @Override
+    public void setIndeterminate(boolean indeterminate) {
+        // not used in command line
     }
 }

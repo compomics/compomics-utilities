17e6f2fc7cdac9f53d6079e7a4fda71b375aac6f
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2013-01-18 20:36:32 +0000

    Fixed a bug in the ModificationProfile where the getSimilarNotFixedModifications method returned duplicate PTMs if a refinement PTM was also added as a variable PTM, later resulting in the crash when loading the data in PeptideShaker. Added stack trace output in IdfileReaderFactory if an exception is thrown. Added the option of using mass ranges for the molecular weight fractions in the SearchParameters. Moved project to version 3.10.49.

--- trunk/src/main/java/com/compomics/util/experiment/io/identifications/IdfileReaderFactory.java
+++ trunk/src/main/java/com/compomics/util/experiment/io/identifications/IdfileReaderFactory.java
@@ -10,7 +10,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.ServiceLoader;
-import java.util.TreeSet;
 
 import org.apache.log4j.Logger;
 import org.xml.sax.SAXException;
@@ -23,26 +22,27 @@ import org.xml.sax.SAXException;
  */
 public class IdfileReaderFactory {
 
-    // Class specific log4j logger for Enzyme instances.
+    /**
+     * Class specific log4j logger for Enzyme instances.
+     */
     static Logger logger = Logger.getLogger(IdfileReaderFactory.class);
-
     /**
      * The factory instance.
      */
     private static IdfileReaderFactory singleton = null;
-
     /**
      * The list of registered IdfileReaders.
      */
     private static HashMap<String, Class> idFileReaders = new HashMap<String, Class>();
 
     /**
-     * Static initializer block that checks for registered IdfileReaders through the Java service loader
+     * Static initializer block that checks for registered IdfileReaders through
+     * the Java service loader
      */
     static {
         ServiceLoader<IdfileReader> ifdrServiceLoader = ServiceLoader.load(IdfileReader.class);
         Iterator<IdfileReader> idfrIterator = ifdrServiceLoader.iterator();
-        
+
         while (idfrIterator.hasNext()) {
             IdfileReader idfileReader = idfrIterator.next();
             logger.info("Found IdfileReader '" + idfileReader.getClass().getCanonicalName() + "' in Java service loader.");
@@ -69,29 +69,33 @@ public class IdfileReaderFactory {
     }
 
     /**
-     * This method registers a new IdfileReader Class, and the file extension it can read from. Note that the
-     * collection of IdfileReaders is keyed by this extension, and similar to the java.util.HashMap syntax, a
-     * Class is therefore returned if the extension provided already had an associated Class.
+     * This method registers a new IdfileReader Class, and the file extension it
+     * can read from. Note that the collection of IdfileReaders is keyed by this
+     * extension, and similar to the java.util.HashMap syntax, a Class is
+     * therefore returned if the extension provided already had an associated
+     * Class.
      *
-     * @param aReader   Class of the IdfileReader to register.
-     * @param aExtension    String with the extension of the file that this IdfileReader implementation can read.
-     * @return  Class with the Class that was already previously registered for this extension, or 'null' if
-     *          the extension was not yet registered at all.
+     * @param aReader Class of the IdfileReader to register.
+     * @param aExtension String with the extension of the file that this
+     * IdfileReader implementation can read.
+     * @return Class with the Class that was already previously registered for
+     * this extension, or 'null' if the extension was not yet registered at all.
      */
     public static Class registerIdFileReader(Class aReader, String aExtension) {
         Class result = null;
         // See if we have the right type of class!
-        if(IdfileReader.class.isAssignableFrom(aReader)) {
+        if (IdfileReader.class.isAssignableFrom(aReader)) {
             // Now verify the presence of a correct constructor!
             try {
                 aReader.getConstructor(File.class);
                 result = idFileReaders.put(aExtension, aReader);
                 logger.info("Registered IdfileReader implementation '" + aReader.getCanonicalName() + "' for extension '" + aExtension + "'.");
-                if(result != null) {
+                if (result != null) {
                     logger.warn("Overwrite occurred for extension '" + aExtension + "'; replaced old IdfileReader '" + result.getCanonicalName() + "' with new IdfileReader '" + aReader.getCanonicalName() + "'!");
                 }
-            } catch(NoSuchMethodException nsme) {
+            } catch (NoSuchMethodException nsme) {
                 logger.warn("Unable to find required constructor with single java.io.File parameter in IdfileReader implementation '" + aReader.getCanonicalName() + "'! IdfileReader is ignored!");
+                nsme.printStackTrace();
             }
         } else {
             logger.warn("Was expecting an implementation of '" + IdfileReader.class.getCanonicalName() + "', but got class '" + aReader.getCanonicalName() + "' instead! Ignoring IdfileReader!");
@@ -102,8 +106,9 @@ public class IdfileReaderFactory {
     /**
      * This method returns the proper identification file reader depending on
      * the format of the provided file. It is very important to close the file
-     * reader after creation. //@TODO: create parsers using waiting handlers and
-     * indexed files.
+     * reader after creation. //
+     *
+     * @TODO: create parsers using waiting handlers and indexed files.
      *
      * @param aFile the file to parse
      * @param waitingHandler a waiting handler to display the results. Can be
@@ -121,24 +126,38 @@ public class IdfileReaderFactory {
         String name = aFile.getName().toLowerCase();
 
         // Iterator registered IdfileReaders, see who likes this file. First come, first served.
-        //@TODO May want to make this more sophisticated, possibly like the DBLoaders in DBToolkit, that get the actual file to read some lines prior to making up their mind; thus constitutes an actual format check rather than an extension check.
+
+        // @TODO: May want to make this more sophisticated, possibly like the DBLoaders in DBToolkit, 
+        //        that get the actual file to read some lines prior to making up their mind; thus constitutes 
+        //        an actual format check rather than an extension check.
+
         Iterator<String> extensions = idFileReaders.keySet().iterator();
         while (extensions.hasNext()) {
             String key = extensions.next();
             String extension = key.toLowerCase();
-            if(name.endsWith(extension)) {
+            if (name.endsWith(extension)) {
                 Class idfileReaderClass = idFileReaders.get(key);
                 try {
                     result = (IdfileReader) idfileReaderClass.getConstructor(File.class).newInstance(aFile);
                     break;
-                } catch(NoSuchMethodException nsme) {
-                    logger.error("Unable to find required constructor with single java.io.File parameter in IdfileReader implementation '" + idfileReaderClass.getCanonicalName() + "', matching query extension '" + extension + "'!", nsme);
-                } catch(IllegalAccessException iae) {
-                    logger.error("Required public constructor with single java.io.File parameter in IdfileReader implementation '" + idfileReaderClass.getCanonicalName() + "', matching query extension '" + extension + "' has incorrect access modifier!", iae);
-                } catch(InvocationTargetException ite) {
-                    logger.error("Required constructor with single java.io.File parameter in IdfileReader implementation '" + idfileReaderClass.getCanonicalName() + "', matching query extension '" + extension + "' threw an exception!", ite);
-                } catch(InstantiationException ie) {
-                    logger.error("Required constructor with single java.io.File parameter in IdfileReader implementation '" + idfileReaderClass.getCanonicalName() + "', matching query extension '" + extension + "' inaccessible; probably abstract class?!", ie);
+                } catch (NoSuchMethodException nsme) {
+                    logger.error("Unable to find required constructor with single java.io.File parameter in IdfileReader implementation '"
+                            + idfileReaderClass.getCanonicalName() + "', matching query extension '" + extension + "'!", nsme);
+                    nsme.printStackTrace();
+                } catch (IllegalAccessException iae) {
+                    logger.error("Required public constructor with single java.io.File parameter in IdfileReader implementation '"
+                            + idfileReaderClass.getCanonicalName() + "', matching query extension '" + extension + "' has incorrect access modifier!", iae);
+                    iae.printStackTrace();
+                } catch (InvocationTargetException ite) {
+                    logger.error("Required constructor with single java.io.File parameter in IdfileReader implementation '"
+                            + idfileReaderClass.getCanonicalName() + "', matching query extension '" + extension + "' threw an exception!", ite);
+                    ite.printStackTrace();
+                } catch (InstantiationException ie) {
+                    logger.error("Required constructor with single java.io.File parameter in IdfileReader implementation '"
+                            + idfileReaderClass.getCanonicalName() + "', matching query extension '" + extension + "' inaccessible; probably abstract class?!", ie);
+                    ie.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
             }
         }

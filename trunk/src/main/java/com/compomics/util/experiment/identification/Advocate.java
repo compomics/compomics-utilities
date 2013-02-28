1ad4f2fbc921c69efecc3a0e8e2db94b022a5326
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2013-02-28 12:33:22 +0000

    Fixed an inconsistency with the search engine names.

--- trunk/src/main/java/com/compomics/util/experiment/identification/Advocate.java
+++ trunk/src/main/java/com/compomics/util/experiment/identification/Advocate.java
@@ -3,54 +3,51 @@ package com.compomics.util.experiment.identification;
 import java.io.Serializable;
 
 /**
- * The advocate of a hit can be a search engine, a rescoring algorithm, etc.
+ * The advocate of a hit can be a search engine, a re-scoring algorithm, etc.
  *
- * Created by IntelliJ IDEA.
- * User: Marc
- * Date: Jun 22, 2010
- * Time: 1:55:22 PM
+ * @author Marc Vaudel
  */
 public interface Advocate extends Serializable {
 
     /**
-     * The version UID for Serialization/Deserialization compatibility
+     * The version UID for Serialization/Deserialization compatibility.
      */
     static final long serialVersionUID = -9081265337103997591L;
     /**
-     * Mascot index
+     * Mascot index.
      */
     public static final int MASCOT = 0;
     /**
-     * OMSSA index
+     * OMSSA index.
      */
     public static final int OMSSA = 1;
     /**
-     * X!Tandem index
+     * X!Tandem index.
      */
     public static final int XTANDEM = 2;
     /**
-     * Peptizer index
+     * Peptizer index.
      */
     public static final int PEPTIZER = 3;
     /**
-     * Andromeda index
+     * Andromeda index.
      */
     public static final int ANDROMEDA = 4;
     /**
-     * PeptideShaker
+     * PeptideShaker.
      */
     public static final int PEPTIDE_SHAKER = 5;
 
     /**
-     * getter for the name of the Advocate
+     * Getter for the name of the Advocate.
      *
      * @return the name of the advocate
      */
     public String getName();
 
     /**
-     * getter for the index of the advocate
-     * 
+     * Getter for the index of the advocate.
+     *
      * @return the index of the advocate
      */
     public int getId();

07b6592edae160e2841b01d5b8fb891272f8f138
mvaudel <mvaudel@f644b618-123a-11df-8689-7786f20063ad>
2013-05-09 00:11:58 +0000

    implemented a row sorter in the self updating tables

--- src/main/java/com/compomics/util/gui/tablemodels/SelfUpdatingTableModel.java
+++ src/main/java/com/compomics/util/gui/tablemodels/SelfUpdatingTableModel.java
@@ -1,13 +1,20 @@
 package com.compomics.util.gui.tablemodels;
 
 import com.compomics.util.gui.waiting.WaitingHandler;
+import com.compomics.util.gui.waiting.waitinghandlers.ProgressDialogX;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import javax.swing.JTable;
 import javax.swing.RowSorter;
 import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
 
 /**
- * These table models include a self updating function.
+ * These table models include a self updating function. Due to instability of
+ * the jtable with this model, it comprises a simple row sorter. Use it at your
+ * own risks and feel free to debug.
  *
  * @author Marc Vaudel
  */
@@ -38,29 +45,29 @@ public abstract class SelfUpdatingTableModel extends DefaultTableModel {
      */
     private LoadingRunnable lastLoadingRunnable = null;
     /**
-     * The row sorter used for the given table. The sorter is used to preload
-     * the data, ignored if null.
+     * List of view indexes
      */
-    public RowSorter sorter = null;
+    private ArrayList<Integer> viewIndexes = null;
     /**
-     * A boolean indicating that the table shall be updated only when all data
-     * is loaded.
+     * indicates which column was last changed
      */
-    private boolean updateWhenComplete = false;
+    private int lastColumnSorted = 0;
 
     /**
-     * Loads the data needed for rows between start and end (inclusively).
+     * Loads the data needed for objects at rows of the given view indexes. Use
+     * this method to cache data before working on the given lines.
      *
-     * @param rows the rows model indexes to load as a list. Shall not be empty
-     * or null.
+     * @param indexes the view indexes to load as a list. Shall not be empty or
+     * null.
      * @param interrupted a boolean indicating whether the loading shall be
      * stopped
      * @return the last updated row
      */
-    protected abstract int loadDataForRows(ArrayList<Integer> rows, boolean interrupted);
+    protected abstract int loadDataForRows(ArrayList<Integer> indexes, boolean interrupted);
 
     /**
-     * Loads the data for a column.
+     * Loads the data for a column. Use this method to cache data before working
+     * on the given column.
      *
      * @param column the column number
      * @param waitingHandler a waiting handler used to display progress to the
@@ -80,14 +87,10 @@ public abstract class SelfUpdatingTableModel extends DefaultTableModel {
      * Calling this method indicates that data is missing at the given row. The
      * data will be loaded in a separate thread and the table updated later on.
      *
-     * @param row the row number
+     * @param row the row number (not the view index)
      * @throws InterruptedException
      */
     protected void dataMissingAtRow(int row) throws InterruptedException {
-
-        if (sorter != null) {
-            row = sorter.convertRowIndexToView(row);
-        }
 
         int anticipatedStart = (int) (row + 0.9 * batchSize);
 
@@ -122,14 +125,8 @@ public abstract class SelfUpdatingTableModel extends DefaultTableModel {
             new Thread(new Runnable() {
                 public synchronized void run() {
                     try {
-                        if (selfUpdating
-                                && (!updateWhenComplete || lastLoadingRunnable.isFinished())) {
-                            SwingUtilities.invokeLater(new Runnable() {
-                                @Override
-                                public void run() {
-                                    fireTableDataChanged();
-                                }
-                            });
+                        if (selfUpdating) {
+                            fireTableDataChanged();
                         }
                     } catch (Exception e) {
                         catchException(e);
@@ -177,44 +174,184 @@ public abstract class SelfUpdatingTableModel extends DefaultTableModel {
     }
 
     /**
-     * Loads the content of a column without updating the table.
-     *
-     * @param columns the column of interest
-     * @param waitingContent the content of a cell indicating that loading is
-     * not completed
-     * @param waitingHandler waiting handler for display of the progress or
-     * loading interruption (can be null)
-     * @throws InterruptedException
+     * Initiates the sorter to the current order of the table
      */
-    public synchronized void loadColumnsContent(ArrayList<Integer> columns, String waitingContent, WaitingHandler waitingHandler) throws InterruptedException {
-        if (waitingHandler != null) {
-            waitingHandler.setSecondaryProgressDialogIndeterminate(false);
-            waitingHandler.setMaxSecondaryProgressValue(columns.size() * getRowCount());
-            waitingHandler.setSecondaryProgressValue(0);
-        }
-        for (int column : columns) {
-            loadDataForColumn(column, waitingHandler);
+    public void initiateSorter() {
+        int nRows = getRowCount();
+        viewIndexes = new ArrayList<Integer>(nRows);
+        for (int row = 0; row < nRows; row++) {
+            viewIndexes.add(row);
         }
     }
 
     /**
-     * Sets the row sorter used for the table. The sorter will be used to
-     * preload data.
+     * Returns the view index of the given row
      *
-     * @param sorter the row sorter used for the table
+     * @param row the row of interest
+     * @return the corresponding view index
      */
-    public void setRowSorter(RowSorter sorter) {
-        this.sorter = sorter;
+    public int getViewIndex(int row) {
+        if (viewIndexes == null) {
+            return row;
+        }
+        int nRows = getRowCount();
+        if (nRows != viewIndexes.size()) {
+            initiateSorter();
+        }
+        if (row < 0 || row >= nRows) {
+            nRows--;
+            throw new IllegalArgumentException("Row " + row + " must be between 0 and " + nRows);
+        }
+        return viewIndexes.get(row);
     }
 
     /**
-     * Sets whether the table content shall be updated only once the loading is
-     * complete.
+     * Returns the row number of the given view index
      *
-     * @param updateWhenComplete
+     * @param viewIndex view index row of interest
+     * @return the corresponding row
      */
-    public void setUpdateWhenComplete(boolean updateWhenComplete) {
-        this.updateWhenComplete = updateWhenComplete;
+    public int getRowNumber(int viewIndex) {
+        if (viewIndexes == null) {
+            return viewIndex;
+        }
+        int nRows = getRowCount();
+        if (nRows != viewIndexes.size()) {
+            initiateSorter();
+        }
+        if (viewIndex < 0 || viewIndex >= nRows) {
+            nRows--;
+            throw new IllegalArgumentException("View index " + viewIndex + " must be between 0 and " + nRows);
+        }
+        return viewIndexes.indexOf(viewIndex);
+    }
+
+    /**
+     * Sorts the table according to a given column using the built in sorter.
+     *
+     * @param column the column of interest
+     * @param progressDialog a progress dialog used to display the progress and
+     * interrupt the process
+     */
+    public void sort(int column, ProgressDialogX progressDialog) {
+
+        if (column == lastColumnSorted) {
+            if (viewIndexes == null || viewIndexes.size() != getRowCount()) {
+                initiateSorter();
+            }
+            Collections.reverse(viewIndexes);
+            fireTableDataChanged();
+        } else {
+
+            final int finalColumn = column;
+            final ProgressDialogX finalProgressDialog = progressDialog;
+            new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        if (finalProgressDialog != null) {
+                            finalProgressDialog.setIndeterminate(false);
+                            finalProgressDialog.setMaxProgressValue(getRowCount());
+                            finalProgressDialog.setValue(0);
+                            finalProgressDialog.setVisible(true);
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        // ignore
+                    }
+                }
+            }, "ProgressDialog").start();
+
+            new Thread("SortThread") {
+                @Override
+                public void run() {
+                    try {
+
+                        setSelfUpdating(false);
+
+                        loadDataForColumn(finalColumn, finalProgressDialog);
+                        
+                        initiateSorter();
+                        lastColumnSorted = 0;
+
+                        HashMap<Comparable, ArrayList<Integer>> valueToRowMap = new HashMap<Comparable, ArrayList<Integer>>();
+                        boolean comparable = false,
+                                string = false;
+                        for (int row = 0; row < getRowCount(); row++) {
+                            Object tableValue = getValueAt(row, finalColumn);
+                            Comparable key;
+                            if (tableValue instanceof Comparable) {
+                                key = (Comparable) tableValue;
+                                comparable = true;
+                            } else {
+                                key = tableValue.toString();
+                                string = true;
+                            }
+                            ArrayList<Integer> rows = valueToRowMap.get(key);
+                            if (rows == null) {
+                                rows = new ArrayList<Integer>();
+                                valueToRowMap.put(key, rows);
+                            }
+                            rows.add(row);
+                            if (finalProgressDialog != null) {
+                                finalProgressDialog.increaseProgressValue();
+                                if (finalProgressDialog.isRunCanceled()) {
+                                    return;
+                                }
+                            }
+                        }
+
+                        ArrayList<Comparable> keys = new ArrayList<Comparable>(valueToRowMap.keySet());
+                        if (string && comparable) {
+                            ArrayList<Comparable> stringValues = new ArrayList<Comparable>();
+                            for (Comparable value : keys) {
+                                stringValues.add(value.toString());
+                            }
+                            keys = stringValues;
+                        }
+
+
+                        if (finalProgressDialog == null || !finalProgressDialog.isRunCanceled()) {
+                            viewIndexes = new ArrayList<Integer>();
+                            Collections.sort(keys);
+                            for (Comparable key : keys) {
+                                viewIndexes.addAll(valueToRowMap.get(key));
+                            }
+                            lastColumnSorted = finalColumn;
+                        }
+
+                    } catch (Exception ex) {
+                        catchException(ex);
+                    } finally {
+                        setSelfUpdating(true);
+                    }
+                    if (finalProgressDialog != null) {
+                        finalProgressDialog.setRunFinished();
+                    }
+                    fireTableDataChanged();
+                }
+            }.start();
+        }
+    }
+
+    /**
+     * Convenience method adding a roe sorter listener to the given JTable
+     *
+     * @param jTable
+     * @param progressDialog progress dialog used to display progress or cancel
+     * while sorting. Can be null.
+     */
+    public static void addSortListener(JTable jTable, ProgressDialogX progressDialog) {
+        final JTableHeader proteinTableHeader = jTable.getTableHeader();
+        final JTable finalTable = jTable;
+        final ProgressDialogX progressDialogX = progressDialog;
+        proteinTableHeader.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseReleased(java.awt.event.MouseEvent evt) {
+                int column = proteinTableHeader.getColumnModel().getColumnIndexAtX(evt.getX());
+                SelfUpdatingTableModel model = (SelfUpdatingTableModel) finalTable.getModel();
+                model.sort(column, progressDialogX);
+            }
+        });
+
+
     }
 
     /**
@@ -234,19 +371,12 @@ public abstract class SelfUpdatingTableModel extends DefaultTableModel {
         @Override
         public synchronized void run() {
             try {
-                ArrayList<Integer> indexes = new ArrayList<Integer>(batchSize);
+                ArrayList<Integer> viewIndexes = new ArrayList<Integer>(batchSize);
                 for (int row = rowStartLoading; row <= rowEndLoading; row++) {
-                    if (sorter == null) {
-                        indexes.add(row);
-                    } else {
-                        indexes.add(sorter.convertRowIndexToModel(row));
-                    }
+                    viewIndexes.add(getViewIndex(row));
                 }
-                if (!indexes.isEmpty() && !interrupted) {
-                    rowEndLoading = loadDataForRows(indexes, interrupted);
-                    if (sorter != null) {
-                        rowEndLoading = sorter.convertRowIndexToView(rowEndLoading);
-                    }
+                if (!viewIndexes.isEmpty() && !interrupted) {
+                    rowEndLoading = getRowNumber(loadDataForRows(viewIndexes, interrupted));
                 }
             } catch (Exception e) {
                 catchException(e);

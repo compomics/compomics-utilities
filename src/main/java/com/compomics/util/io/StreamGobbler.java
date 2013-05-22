aedb1aa6386476d2ff55611d87989330a0f37beb
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2013-05-22 16:06:26 +0000

    Corrected a typo statup.log > startup.log. Added JavaDoc.

--- src/main/java/com/compomics/util/io/StreamGobbler.java
+++ src/main/java/com/compomics/util/io/StreamGobbler.java
@@ -7,14 +7,32 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
- *
- * @author Davy
+ * A stream gobbler.
+ * 
+ * @author Davy Maddelein
  */
 public class StreamGobbler extends Thread {
+
+    /**
+     * The input stream.
+     */
     private InputStream is;
+    /**
+     * The stream type.
+     */
     private String type;
+    /**
+     * The buffered writer.
+     */
     private BufferedWriter bw;
 
+    /**
+     * Constructor.
+     * 
+     * @param is the input stream
+     * @param type the stream type
+     * @param bw the buffered writer
+     */
     public StreamGobbler(InputStream is, String type, BufferedWriter bw) {
         this.is = is;
         this.type = type;
@@ -27,13 +45,11 @@ public class StreamGobbler extends Thread {
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
             String line = null;
-            while ((line = br.readLine()) != null)
-                bw.write(type + ": "+line);
-        }
-        catch (IOException ioe) {
+            while ((line = br.readLine()) != null) {
+                bw.write(type + ": " + line);
+            }
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
     }
-    
-    
 }

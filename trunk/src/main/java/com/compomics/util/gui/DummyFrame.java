ccac61b93e95849f2005910baf981888eba7de0b
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2013-06-09 15:52:21 +0000

    Moved the DummyFrame class from PeptideShaker. If the WaitingDialog has a dummy frame parent this parent will now also be closed i the WaitingDialog closes. Moved project to version 3.13.36.

---
+++ trunk/src/main/java/com/compomics/util/gui/DummyFrame.java
@@ -0,0 +1,40 @@
+package com.compomics.util.gui;
+
+import java.awt.Toolkit;
+import java.util.Arrays;
+import javax.swing.JFrame;
+
+/**
+ * A simple class used to be able to show a JDialog without a parent frame in
+ * the OS task bar.
+ *
+ * @author Harald Barsnes
+ */
+public class DummyFrame extends JFrame {
+
+    /**
+     * Constructor.
+     *
+     * @param title the frame title
+     * @param relativeImageIconPath the relative path to the default icon for
+     * the frame
+     */
+    public DummyFrame(String title, String relativeImageIconPath) {
+        super(title);
+        setUndecorated(true);
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setIconImages(Arrays.asList(Toolkit.getDefaultToolkit().getImage(getClass().getResource(relativeImageIconPath))));
+    }
+
+    /**
+     * Update the frame title and return the frame.
+     *
+     * @param title the new title
+     * @return the updated dummy frame
+     */
+    public DummyFrame setNewTitle(String title) {
+        this.setTitle(title);
+        return this;
+    }
+}

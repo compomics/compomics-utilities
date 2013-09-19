b63afa5479b91f148d2660620bfe37148f7dae9b
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2013-09-19 10:05:33 +0000

    Added PRIDE to utilities mapping for TMT duplex.

--- trunk/src/main/java/com/compomics/util/preferences/IdFilter.java
+++ trunk/src/main/java/com/compomics/util/preferences/IdFilter.java
@@ -125,9 +125,9 @@ public class IdFilter implements Serializable {
         int searchEngine = assumption.getAdvocate();
         double eValue = assumption.getScore();
 
-        if (searchEngine == Advocate.MASCOT && mascotMaxEvalue > 0 && eValue > mascotMaxEvalue
-                || searchEngine == Advocate.OMSSA && omssaMaxEvalue > 0 && eValue > omssaMaxEvalue
-                || searchEngine == Advocate.XTANDEM && xtandemMaxEvalue > 0 && eValue > xtandemMaxEvalue) {
+        if ((searchEngine == Advocate.MASCOT && mascotMaxEvalue > 0 && eValue > mascotMaxEvalue)
+                || (searchEngine == Advocate.OMSSA && omssaMaxEvalue > 0 && eValue > omssaMaxEvalue)
+                || (searchEngine == Advocate.XTANDEM && xtandemMaxEvalue > 0 && eValue > xtandemMaxEvalue)) {
             return false;
         }
         return true;

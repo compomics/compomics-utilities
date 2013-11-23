83081a5ec35751027b0d66f8d5b973befa85b2df
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2013-11-23 17:27:25 +0000

    Added a simple FTP file downloader. Made the getSpectrumFile method in the Spectrum class faster by replacing the use of the split method, as this required the compiling if a regular expression every time the method was used. Added commons-net as a dependency. Moved project to version 3.18.4.

--- src/main/java/com/compomics/util/experiment/massspectrometry/Spectrum.java
+++ src/main/java/com/compomics/util/experiment/massspectrometry/Spectrum.java
@@ -67,7 +67,7 @@ public abstract class Spectrum extends ExperimentObject {
      * @return the name of the file containing the spectrum
      */
     public static String getSpectrumFile(String spectrumKey) {
-        return spectrumKey.split(SPECTRUM_KEY_SPLITTER)[0];
+        return spectrumKey.substring(0, spectrumKey.indexOf(SPECTRUM_KEY_SPLITTER)).trim();
     }
 
     /**
@@ -78,7 +78,7 @@ public abstract class Spectrum extends ExperimentObject {
      * @return the title of the spectrum
      */
     public static String getSpectrumTitle(String spectrumKey) {
-        return spectrumKey.substring(spectrumKey.indexOf(SPECTRUM_KEY_SPLITTER) + 5).trim();
+        return spectrumKey.substring(spectrumKey.indexOf(SPECTRUM_KEY_SPLITTER) + SPECTRUM_KEY_SPLITTER.length()).trim();
     }
 
     /**

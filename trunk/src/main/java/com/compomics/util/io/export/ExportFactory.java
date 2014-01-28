f36813d0e872c567ded74b187be28c7f8dca2dc7
mvaudel <mvaudel@f644b618-123a-11df-8689-7786f20063ad>
2014-01-28 10:14:31 +0000

    moved the report creation to utilities

---
+++ trunk/src/main/java/com/compomics/util/io/export/ExportFactory.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.compomics.util.io.export;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * An export factory manages and generates reports
+ *
+ * @author Marc
+ */
+public interface ExportFactory extends Serializable {
+    
+
+    /**
+     * Returns the export scheme indexed by the given name.
+     *
+     * @param schemeName the name of the desired export scheme
+     * @return the desired export scheme
+     */
+    public ExportScheme getExportScheme(String schemeName);
+    /**
+     * Removes a user scheme.
+     *
+     * @param schemeName the name of the scheme to remove
+     */
+    public void removeExportScheme(String schemeName);
+
+    /**
+     * Adds an export scheme to the map of user schemes.
+     *
+     * @param exportScheme the new export scheme, will be accessible via its
+     * name
+     */
+    public void addExportScheme(ExportScheme exportScheme);
+
+    /**
+     * Returns the implemented sections.
+     *
+     * @return the implemented sections
+     */
+    public ArrayList<String> getImplementedSections();
+    /**
+     * Returns the export features implemented for the given section.
+     * 
+     * @param sectionName the name of the sectoin of interest
+     * 
+     * @return a list of export features
+     */
+    public ArrayList<ExportFeature> getExportFeatures(String sectionName);
+}

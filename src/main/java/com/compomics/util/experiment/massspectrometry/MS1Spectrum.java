78d337d9761bb89d3c962348413059d1c522567b
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2014-03-04 14:00:08 +0000

    Minor JavaDoc fixes.

--- src/main/java/com/compomics/util/experiment/massspectrometry/MS1Spectrum.java
+++ src/main/java/com/compomics/util/experiment/massspectrometry/MS1Spectrum.java
@@ -1,31 +1,27 @@
 package com.compomics.util.experiment.massspectrometry;
 
 import java.util.HashMap;
-import java.util.HashSet;
 
 /**
  * This class models an MS1 spectrum.
  *
- * Created by IntelliJ IDEA.
- * User: Marc
- * Date: Jun 18, 2010
- * Time: 9:04:58 AM
+ * @author Marc Vaudel
  */
 public class MS1Spectrum extends Spectrum {
 
     /**
-     * The version UID for Serialization/Deserialization compatibility
+     * The version UID for Serialization/Deserialization compatibility.
      */
     static final long serialVersionUID = -7328539274222920271L;
 
     /**
-     * Constructor for an MS1 spectrum
+     * Constructor for an MS1 spectrum.
      */
     public MS1Spectrum() {
     }
 
     /**
-     * constructor for an MS1 spectrum
+     * Constructor for an MS1 spectrum.
      *
      * @param spectrumTitle     title of the spectrum
      * @param spectrum          Set of peaks

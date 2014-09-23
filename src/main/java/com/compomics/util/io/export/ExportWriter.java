e4677f10efbd51c156bce4127d0f5d4c62dbec41
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2014-09-23 22:33:54 +0000

    Minor JavaDoc fixes.

--- src/main/java/com/compomics/util/io/export/ExportWriter.java
+++ src/main/java/com/compomics/util/io/export/ExportWriter.java
@@ -9,158 +9,163 @@ import java.io.IOException;
 /**
  * The export writer will write the output in the desired export format.
  *
- * @author Marc
+ * @author Marc Vaudel
  */
 public abstract class ExportWriter {
-    
+
     /**
-     * The format of the export
+     * The format of the export.
      */
     protected ExportFormat exportFormat;
 
     /**
      * Returns the export of the format.
-     * 
+     *
      * @return the export of the format
      */
     public ExportFormat getExportFormat() {
         return exportFormat;
     }
-    
+
     /**
      * Writes text to the export.
-     * 
+     *
      * @param text the text to write
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public void write(String text) throws IOException {
         write(text, null);
     }
-    
+
     /**
      * Writes text to the export.
-     * 
+     *
      * @param text the text to write
      * @param textStyle the style to use, overwrites any previous/default
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public abstract void write(String text, WorkbookStyle textStyle) throws IOException;
-    
+
     /**
      * Writes the main title.
-     * 
+     *
      * @param text the text to write
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public void writeMainTitle(String text) throws IOException {
         writeMainTitle(text, null);
     }
-    
+
     /**
      * Writes the main title.
-     * 
+     *
      * @param text the text to write
      * @param textStyle the style to use, overwrites any previous/default
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public abstract void writeMainTitle(String text, WorkbookStyle textStyle) throws IOException;
-    
+
     /**
      * Starts a new section.
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public void startNewSection() throws IOException {
         startNewSection(null, null);
     }
-    
+
     /**
      * Starts a new section.
-     * 
+     *
      * @param sectionTitle the text to write
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public void startNewSection(String sectionTitle) throws IOException {
         startNewSection(sectionTitle, null);
     }
-    
+
     /**
      * Starts a new section.
-     * 
+     *
      * @param sectionTitle the text to write
      * @param textStyle the style to use, overwrites any previous/default
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public abstract void startNewSection(String sectionTitle, WorkbookStyle textStyle) throws IOException;
-    
+
     /**
      * Writes header text to the export.
-     * 
+     *
      * @param text the text to write
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public void writeHeaderText(String text) throws IOException {
         writeHeaderText(text, null);
     }
-    
+
     /**
      * Writes header text to the export.
-     * 
+     *
      * @param text the text to write
      * @param textStyle the style to use, overwrites any previous/default
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public abstract void writeHeaderText(String text, WorkbookStyle textStyle) throws IOException;
-    
+
     /**
      * Adds a separator.
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public abstract void addSeparator() throws IOException;
-    
+
     /**
      * Adds a separator.
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public abstract void newLine() throws IOException;
-    
+
     /**
      * Writes the content in cache and closes the connection to the file.
-     * 
+     *
      * @throws java.io.IOException
      * @throws java.io.FileNotFoundException
      */
     public abstract void close() throws IOException, FileNotFoundException;
-    
+
     /**
-     * Notifies the writer that data of a higher hierarchical depth will be written, e.g. going from protein to peptide.
+     * Notifies the writer that data of a higher hierarchical depth will be
+     * written, e.g. going from protein to peptide.
      */
     public abstract void increaseDepth();
+
     /**
-     * Notifies the writer that data of a lower hierarchical depth will be written, e.g. going from peptide to protein.
+     * Notifies the writer that data of a lower hierarchical depth will be
+     * written, e.g. going from peptide to protein.
      */
     public abstract void decreseDepth();
+
     /**
      * Returns an export writer for the desired format.
-     * 
+     *
      * @param exportFormat the export format
      * @param destinationFile the file where to write the export
      * @param separator the separator for a text export
-     * @param nSeparationLines the number of separation lines between two sections for a text export
-     * 
+     * @param nSeparationLines the number of separation lines between two
+     * sections for a text export
+     *
      * @return an export writer for the desired format
-     * 
-     * @throws IOException 
+     *
+     * @throws IOException
      */
     public static ExportWriter getExportWriter(ExportFormat exportFormat, File destinationFile, String separator, int nSeparationLines) throws IOException {
         switch (exportFormat) {
@@ -171,6 +176,5 @@ public abstract class ExportWriter {
             default:
                 throw new IllegalArgumentException("No exporter implemented for format " + exportFormat.name + ".");
         }
-        
     }
 }
--- src/main/java/com/compomics/util/io/export/WorkbookStyle.java
+++ src/main/java/com/compomics/util/io/export/WorkbookStyle.java
@@ -5,69 +5,69 @@ import org.apache.poi.ss.usermodel.CellStyle;
 /**
  * This interface sets the style of a workbook export.
  *
- * @author Marc
+ * @author Marc Vaudel
  */
 public interface WorkbookStyle {
 
     /**
      * Returns the cell style for the main title.
-     * 
+     *
      * @return the cell style for the main title
      */
     public CellStyle getMainTitleStyle();
-    
+
     /**
      * Returns the row height for the main title.
-     * 
+     *
      * @return the row height for the main title
      */
     public float getMainTitleRowHeight();
 
     /**
      * Returns the standard cell style.
-     * 
+     *
      * @return the standard cell style
      */
     public CellStyle getStandardStyle();
 
     /**
-     * Returns the standard cell style for the given hierarchical depth. The depth is defined by the sublevel of the current cell.
-     * 
+     * Returns the standard cell style for the given hierarchical depth. The
+     * depth is defined by the sublevel of the current cell.
+     *
      * @param hierarchicalDepth the hierarchical depth
-     * 
+     *
      * @return the standard cell style for the given hierarchical depth
      */
     public CellStyle getStandardStyle(int hierarchicalDepth);
-    
+
     /**
      * Returns the height of a standard line.
-     * 
+     *
      * @return the height of a standard line
      */
     public float getStandardHeight();
 
     /**
      * Returns the standard cell style of a header.
-     * 
+     *
      * @return the standard cell style of a header
      */
     public CellStyle getHeaderStyle();
 
     /**
-     * Returns the standard cell style of a header at the given hierarchical depth. The depth is defined by the sublevel of the current cell.
-     * 
+     * Returns the standard cell style of a header at the given hierarchical
+     * depth. The depth is defined by the sublevel of the current cell.
+     *
      * @param hierarchicalDepth the hierarchical depth
-     * 
+     *
      * @return the standard cell style for the given hierarchical depth
      */
     public CellStyle getHeaderStyle(int hierarchicalDepth);
-    
+
     /**
      * Returns the height of a header row.
-     * 
+     *
      * @return the height of a header row
      */
     public float getHeaderHeight();
-    
-    
 }

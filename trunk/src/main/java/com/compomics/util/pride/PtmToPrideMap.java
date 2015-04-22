9a6c026286f91eed1d01b3904f04d370ceec5190
harald.barsnes <harald.barsnes@f644b618-123a-11df-8689-7786f20063ad>
2015-04-22 16:26:13 +0000

    Fixed a bug in the CV term mapping for pyro-cmc. Moved project to version 3.47.4.

--- trunk/src/main/java/com/compomics/util/pride/PtmToPrideMap.java
+++ trunk/src/main/java/com/compomics/util/pride/PtmToPrideMap.java
@@ -246,9 +246,10 @@ public class PtmToPrideMap implements Serializable {
         } else if (ptmName.equalsIgnoreCase("pyridyl K")
                 || ptmName.equalsIgnoreCase("pyridyl peptide n-term")) {
             return new CvTerm("UNIMOD", "UNIMOD:25", "Pyridylacetyl", "119.037114");
-        } else if (ptmName.equalsIgnoreCase("pyro-cmC")
-                || ptmName.equalsIgnoreCase("pyro-glu from n-term Q")) {
+        } else if (ptmName.equalsIgnoreCase("pyro-glu from n-term Q")) {
             return new CvTerm("UNIMOD", "UNIMOD:28", "Gln->pyro-Glu", "-17.026549");
+        } else if (ptmName.equalsIgnoreCase("pyro-cmc")) {
+            return new CvTerm("UNIMOD", "UNIMOD:385", "Ammonia-loss ", "-17.026549");
         } else if (ptmName.equalsIgnoreCase("pyro-glu from n-term E")) {
             return new CvTerm("UNIMOD", "UNIMOD:27", "Glu->pyro-Glu", "-18.010565");
         } else if (ptmName.equalsIgnoreCase("oxidation of P to pyroglutamic acid")) {

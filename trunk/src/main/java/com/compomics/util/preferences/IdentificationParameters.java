8e23a27ce0d8bb3749918390a1f0f9e05db5f0aa
mvaudel@gmail.com <mvaudel@gmail.com@f644b618-123a-11df-8689-7786f20063ad>
2015-06-09 16:11:36 +0000

    added a convenience method to the identification parameters.

--- trunk/src/main/java/com/compomics/util/preferences/IdentificationParameters.java
+++ trunk/src/main/java/com/compomics/util/preferences/IdentificationParameters.java
@@ -69,6 +69,32 @@ public class IdentificationParameters implements Serializable {
     public void setSearchParameters(SearchParameters searchParameters) {
         this.searchParameters = searchParameters;
     }
+    
+    /**
+     * Returns the relative tolerance in ppm corresponding to the absolute tolerance in dalton at the given reference mass.
+     * 
+     * @param daltonTolerance the absolute tolerance in dalton
+     * @param refMass the reference mass in dalton
+     * 
+     * @return the relative tolerance in ppm
+     */
+    public static double getPpmTolerance(double daltonTolerance, double refMass) {
+        double result = daltonTolerance / refMass * 1000000;
+        return result;
+    }
+    
+    /**
+     * Returns the absolute tolerance in dalton corresponding to the relative tolerance in ppm at the given reference mass.
+     * 
+     * @param ppmTolerance the absolute tolerance in ppm
+     * @param refMass the reference mass in dalton
+     * 
+     * @return the relative tolerance in ppm
+     */
+    public static double getDaTolerance(double ppmTolerance, double refMass) {
+        double result = ppmTolerance / 1000000 * refMass;
+        return result;
+    }
 
     /**
      * Returns the annotation preferences used for identification.

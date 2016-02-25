85d3f51dcf853691c2b3fcfe069c63d49aa0b0da
Harald Barsnes <harald.barsnes@gmail.com>
2016-02-25 22:28:40 +0100

    Minor JavaDoc cleanups.

--- src/main/java/com/compomics/util/pride/PrideWebService.java
+++ src/main/java/com/compomics/util/pride/PrideWebService.java
@@ -15,25 +15,25 @@ import uk.ac.ebi.pride.archive.web.service.model.project.ProjectSummaryList;
 import uk.ac.ebi.pride.archive.web.service.model.protein.ProteinDetailList;
 
 /**
- * A java representation of the PRIDE web service
+ * A java representation of the PRIDE web service.
  *
  * @author Kenneth Verheggen
  */
 public class PrideWebService {
 
     /**
-     * The pride json marshaller to interpret json objects
+     * The pride json marshaller to interpret json objects.
      */
-    private static final PrideMarshaller marshaller = new PrideMarshaller();
+    private static final PrideMarshaller MARSHALLER = new PrideMarshaller();
     /**
      * The page size that is to be used for buffering large lists (if the query
-     * supports this)
+     * supports this).
      */
-    private static final int pageSize = 1000;
+    private static final int PAGE_SIZE = 1000;
 
     /**
      * Returns the reachable query for a given root + query + pride filter
-     * instances
+     * instances.
      *
      * @param queryRoot the root for the query
      * @param query the criteria for the query
@@ -56,7 +56,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a count for the given query
+     * Returns a count for the given query.
      *
      * @param query the query that delivers objects that should be counted
      * @return the count for the given query
@@ -64,12 +64,11 @@ public class PrideWebService {
      * was invalid
      */
     private static int getCount(String query) throws IOException {
-        return (Integer) marshaller.fromJson(Integer.class, new URL(query.replace("list", "count")));
+        return (Integer) MARSHALLER.fromJson(Integer.class, new URL(query.replace("list", "count")));
     }
 
-    //project info  
     /**
-     * Returns a ProjectSummaryList for the given query and filters
+     * Returns a ProjectSummaryList for the given query and filters.
      *
      * @param query the criteria for a search
      * @param filters the filters for a search
@@ -84,7 +83,7 @@ public class PrideWebService {
         boolean moreResults = true;
         int page = 0;
         while (moreResults) {
-            ProjectSummaryList subSummary = marshaller.getProjectSummaryList(projectListURL + "&page=" + page);
+            ProjectSummaryList subSummary = MARSHALLER.getProjectSummaryList(projectListURL + "&page=" + page);
             if (subSummary == null || subSummary.getList().isEmpty()) {
                 moreResults = false;
             } else {
@@ -96,7 +95,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a project count for the given criteria and filters
+     * Returns a project count for the given criteria and filters.
      *
      * @param query the criteria for the search
      * @param filters the filters for the search
@@ -109,7 +108,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns the project details for a given accession
+     * Returns the project details for a given accession.
      *
      * @param projectAccession the project accession
      * @return the project details for a given accession
@@ -118,12 +117,11 @@ public class PrideWebService {
      */
     public static ProjectDetail getProjectDetail(String projectAccession) throws IOException {
         URL URL = new URL(PrideQuery.GET_PROJECT_BY_ACCESSION.getQueryTemplate(false).replace("{projectAccession}", projectAccession));
-        return (ProjectDetail) marshaller.fromJson(ProjectDetail.class, URL);
+        return (ProjectDetail) MARSHALLER.fromJson(ProjectDetail.class, URL);
     }
 
-    //assay info
     /**
-     * Returns the assay details for a given project
+     * Returns the assay details for a given project.
      *
      * @param projectAccession the project accession
      * @return the assay details for a given project
@@ -133,12 +131,12 @@ public class PrideWebService {
     public static AssayDetailList getAssayDetails(String projectAccession) throws IOException {
         //assays
         String assayListURL = PrideQuery.GET_ASSAYS_BY_PROJECT.getQueryTemplate(false).replace("{projectAccession}", projectAccession);
-        AssayDetailList assayDetails = (AssayDetailList) marshaller.getAssayDetailList(assayListURL);
+        AssayDetailList assayDetails = (AssayDetailList) MARSHALLER.getAssayDetailList(assayListURL);
         return assayDetails;
     }
 
     /**
-     * Returns the assay count for a given project
+     * Returns the assay count for a given project.
      *
      * @param projectAccession the project accession
      * @return the assay count for a given project
@@ -150,7 +148,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns the assay details for a given assay
+     * Returns the assay details for a given assay.
      *
      * @param assayAccession the project assayAccession
      * @return the assay details for a given assay
@@ -159,12 +157,11 @@ public class PrideWebService {
      */
     public static AssayDetail getAssayDetail(String assayAccession) throws IOException {
         URL URL = new URL(PrideQuery.GET_ASSAY_BY_ACCESSION.getQueryTemplate(false).replace("{assayAccession}", assayAccession));
-        return (AssayDetail) marshaller.fromJson(AssayDetail.class, URL);
+        return (AssayDetail) MARSHALLER.fromJson(AssayDetail.class, URL);
     }
 
-    //file info
     /**
-     * Returns a list of file details for a given project
+     * Returns a list of file details for a given project.
      *
      * @param projectAccession the project accession
      * @return a list of file details for a given project
@@ -173,12 +170,12 @@ public class PrideWebService {
      */
     public static FileDetailList getProjectFileDetails(String projectAccession) throws IOException {
         String fileListURL = PrideQuery.GET_FILES_BY_PROJECT.getQueryTemplate(false).replace("{projectAccession}", projectAccession);
-        FileDetailList files = marshaller.getFileDetailList(fileListURL);
+        FileDetailList files = MARSHALLER.getFileDetailList(fileListURL);
         return files;
     }
 
     /**
-     * Returns a count of file details for a given project
+     * Returns a count of file details for a given project.
      *
      * @param projectAccession the project accession
      * @return a count of file details for a given project
@@ -190,7 +187,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a list of file details for a given assay
+     * Returns a list of file details for a given assay.
      *
      * @param assayAccession the assay accession
      * @return a list of file details for a given assay
@@ -199,12 +196,12 @@ public class PrideWebService {
      */
     public static FileDetailList getAssayFileDetails(String assayAccession) throws IOException {
         String fileListURL = PrideQuery.GET_FILES_BY_ASAY.getQueryTemplate(false).replace("{assayAccession}", assayAccession);
-        FileDetailList fileDetails = marshaller.getFileDetailList(fileListURL);
+        FileDetailList fileDetails = MARSHALLER.getFileDetailList(fileListURL);
         return fileDetails;
     }
 
     /**
-     * Returns a count of file details for a given assay
+     * Returns a count of file details for a given assay.
      *
      * @param assayAccession the assay accession
      * @return a count of file details for a given assay
@@ -215,9 +212,8 @@ public class PrideWebService {
         return getCount(PrideQuery.GET_FILES_BY_ASAY.getQueryTemplate(false).replace("{assayAccession}", assayAccession));
     }
 
-    //protein info
     /**
-     * Returns a list of proteins, gotten from the paged query
+     * Returns a list of proteins, gotten from the paged query.
      *
      * @param queryURL the root query url
      * @return a list of proteins, gotten from the paged query
@@ -230,7 +226,7 @@ public class PrideWebService {
         boolean moreResults = true;
         int page = 0;
         while (moreResults) {
-            ProteinDetailList subList = (ProteinDetailList) marshaller.getProteinDetailList(queryURL + "?show=" + pageSize + "&page=" + page);
+            ProteinDetailList subList = (ProteinDetailList) MARSHALLER.getProteinDetailList(queryURL + "?show=" + PAGE_SIZE + "&page=" + page);
             if (subList.getList().isEmpty()) {
                 moreResults = false;
             } else {
@@ -242,7 +238,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a list of protein details for a given project
+     * Returns a list of protein details for a given project.
      *
      * @param projectAccession the project accession
      * @return a list of protein details for a given project
@@ -255,7 +251,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a count of protein details for a given project
+     * Returns a count of protein details for a given project.
      *
      * @param projectAccession the project accession
      * @return a count of protein details for a given project
@@ -268,7 +264,7 @@ public class PrideWebService {
 
     /**
      * Returns a list of protein details for a given project and a given protein
-     * accession
+     * accession.
      *
      * @param projectAccession the project accession
      * @param proteinAccesion the protein accession
@@ -281,13 +277,13 @@ public class PrideWebService {
         String proteinIdentificationList = PrideQuery.GET_PROTEIN_IDENTIFICATION_BY_PROJECT.getQueryTemplate(false)
                 .replace("{projectAccession}", projectAccession)
                 .replace("{accession}", proteinAccesion);
-        return (ProteinDetailList) marshaller.getProteinDetailList(proteinIdentificationList);
+        return (ProteinDetailList) MARSHALLER.getProteinDetailList(proteinIdentificationList);
 
     }
 
     /**
      * Returns a count of protein details for a given project and a given
-     * protein accession
+     * protein accession.
      *
      * @param projectAccession the project accession
      * @param proteinAccession the protein accession
@@ -304,7 +300,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a list of protein details for a given assay
+     * Returns a list of protein details for a given assay.
      *
      * @param assayAccession the assay accession
      * @return a list of protein details for a given assay
@@ -314,11 +310,11 @@ public class PrideWebService {
     public static ProteinDetailList getProteinIdentificationByAssay(String assayAccession) throws IOException {
         String proteinIdentificationList = PrideQuery.GET_ALL_PROTEIN_IDENTIFICATIONS_BY_ASSAY.getQueryTemplate(false)
                 .replace("{assayAccession}", assayAccession);
-        return (ProteinDetailList) marshaller.getProteinDetailList(proteinIdentificationList);
+        return (ProteinDetailList) MARSHALLER.getProteinDetailList(proteinIdentificationList);
     }
 
     /**
-     * Returns a count of protein details for a given assay
+     * Returns a count of protein details for a given assay.
      *
      * @param assayAccession the assay accession
      * @return a count of protein details for a given assay
@@ -331,12 +327,11 @@ public class PrideWebService {
         return getCount(proteinIdentificationList);
     }
 
-    //peptide info
     /**
      * Returns a list of peptide to spectrum matches, gotten from the paged
-     * query
+     * query.
      *
-     * @param queryURL the root query url
+     * @param queryURL the root query URL
      * @return a list of peptide to spectrum matches, gotten from the paged
      * query
      * @throws IOException thrown if the webservice is down or the json response
@@ -348,7 +343,7 @@ public class PrideWebService {
         boolean moreResults = true;
         int page = 0;
         while (moreResults) {
-            PsmDetailList subList = (PsmDetailList) marshaller.getPeptideDetailList(queryURL + "?show=" + pageSize + "&page=" + page);
+            PsmDetailList subList = (PsmDetailList) MARSHALLER.getPeptideDetailList(queryURL + "?show=" + PAGE_SIZE + "&page=" + page);
             if (subList.getList().isEmpty()) {
                 moreResults = false;
             } else {
@@ -360,7 +355,7 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a list of psm details for a given project
+     * Returns a list of PSM details for a given project.
      *
      * @param projectAccession the project accession
      * @return a list of protein details for a given project
@@ -375,10 +370,10 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a count of psm details for a given project
+     * Returns a count of PSM details for a given project.
      *
      * @param projectAccession the project accession
-     * @return a count of psm details for a given project
+     * @return a count of PSM details for a given project
      * @throws IOException thrown if the webservice is down or the json response
      * was invalid
      */
@@ -388,12 +383,12 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a list of psm details for a given project and a given peptide
-     * sequence
+     * Returns a list of PSM details for a given project and a given peptide
+     * sequence.
      *
      * @param projectAccession the project accession
      * @param sequence the peptide sequence
-     * @return a list of psm details for a given project and a given peptide
+     * @return a list of PSM details for a given project and a given peptide
      * sequence
      * @throws IOException thrown if the webservice is down or the json response
      * was invalid
@@ -403,16 +398,16 @@ public class PrideWebService {
         String peptideIdentificationList = PrideQuery.GET_ALL_PEPTIDE_IDENTIFICATIONS_BY_PROJECT_AND_SEQUENCE.getQueryTemplate(false)
                 .replace("{projectAccession}", projectAccession)
                 .replace("{peptideSequence}", sequence);
-        return (PsmDetailList) marshaller.getPeptideDetailList(peptideIdentificationList);
+        return (PsmDetailList) MARSHALLER.getPeptideDetailList(peptideIdentificationList);
     }
 
     /**
-     * Returns a count of psm details for a given project and a given peptide
-     * sequence
+     * Returns a count of PSM details for a given project and a given peptide
+     * sequence.
      *
      * @param projectAccession the project accession
      * @param sequence the peptide sequence
-     * @return a count of psm details for a given project and a given peptide
+     * @return a count of PSM details for a given project and a given peptide
      * sequence
      * @throws IOException thrown if the webservice is down or the json response
      * was invalid
@@ -424,25 +419,24 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a list of psm details for a given assay
+     * Returns a list of PSM details for a given assay.
      *
      * @param assayAccession the project accession
-     * @return a list of psm details for a given assay
+     * @return a list of PSM details for a given assay
      * @throws IOException thrown if the webservice is down or the json response
      * was invalid
      */
     public static PsmDetailList getPSMsByAssay(String assayAccession) throws IOException {
-        //assays
         String peptideIdentificationList = PrideQuery.GET_ALL_PEPTIDE_IDENTIFICATIONS_BY_ASSAY.getQueryTemplate(false)
                 .replace("{assayAccession}", assayAccession);
         return bufferPSMs(peptideIdentificationList);
     }
 
     /**
-     * Returns a count of psm details for a given assay
+     * Returns a count of PSM details for a given assay.
      *
      * @param assayAccession the project accession
-     * @return a count of psm details for a given assay
+     * @return a count of PSM details for a given assay
      * @throws IOException thrown if the webservice is down or the json response
      * was invalid
      */
@@ -452,31 +446,30 @@ public class PrideWebService {
     }
 
     /**
-     * Returns a list of psm details for a given assay and a given peptide
-     * sequence
+     * Returns a list of PSM details for a given assay and a given peptide
+     * sequence.
      *
      * @param assayAccession the project accession
      * @param sequence the peptide sequence
-     * @return a list of psm details for a given assay and a given peptide
+     * @return a list of PSM details for a given assay and a given peptide
      * sequence
      * @throws IOException thrown if the webservice is down or the json response
      * was invalid
      */
     public static PsmDetailList getPSMsByAssayAndSequence(String assayAccession, String sequence) throws IOException {
-        //assays
         String peptideIdentificationList = PrideQuery.GET_ALL_PEPTIDE_IDENTIFICATIONS_BY_ASSAY_AND_SEQUENCE.getQueryTemplate(false)
                 .replace("{assayAccession}", assayAccession)
                 .replace("{peptideSequence}", sequence);
-        return (PsmDetailList) marshaller.getPeptideDetailList(peptideIdentificationList);
+        return (PsmDetailList) MARSHALLER.getPeptideDetailList(peptideIdentificationList);
     }
 
     /**
-     * Returns a count of psm details for a given assay and a given peptide
-     * sequence
+     * Returns a count of PSM details for a given assay and a given peptide
+     * sequence.
      *
      * @param assayAccession the project accession
      * @param sequence the peptide sequence
-     * @return a count of psm details for a given assay and a given peptide
+     * @return a count of PSM details for a given assay and a given peptide
      * sequence
      * @throws IOException thrown if the webservice is down or the json response
      * was invalid

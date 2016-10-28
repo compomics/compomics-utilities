7f15b2d6a66eb9c4729e91d8aef5fe1c7e3899ef
Harald Barsnes <harald.barsnes@gmail.com>
2016-10-28 16:25:34 +0200

    Updated Ensembl genomes to version 33.

--- src/main/java/com/compomics/util/experiment/biology/genes/ensembl/EnsemblVersion.java
+++ src/main/java/com/compomics/util/experiment/biology/genes/ensembl/EnsemblVersion.java
@@ -22,7 +22,7 @@ public class EnsemblVersion {
 
         // @TODO: find a less hard coded way of finding the current ensembl versions!!!
         if (ensemblGenomeDivision != null) {
-            return 32;
+            return 33;
         } else {
             return 86;
         }

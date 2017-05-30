39e9031a61cec8e5f26867b050d3907bd4f77cc5
dominik.kopczynski <dominik.kopczynski@isas.de>
2017-05-30 15:23:45 +0200

    fixed getAminoAcidPatternFromString issue

--- src/main/java/com/compomics/util/experiment/biology/AminoAcidPattern.java
+++ src/main/java/com/compomics/util/experiment/biology/AminoAcidPattern.java
@@ -170,65 +170,10 @@ public class AminoAcidPattern extends ExperimentObject implements TagComponent {
                     ++pos;
                 }
             }
-            
-            
-            
-            
-            
-            
-            
-            /*
-            
-            Matcher matcher = Pattern.compile("\\[([^\\]]+)").matcher(aminoAcidPatternAsString);
-            int pos = -1, siteIndex = -startIndex, numSites = 0, numAAs = 0;
-            while (matcher.find(pos + 1)){
-                pos = matcher.start();
-                ArrayList<Character> aminoAcids = new ArrayList<Character>();
-                for (char c : matcher.group(1).toCharArray()){
-                    AminoAcid.getAminoAcid(c);
-                    aminoAcids.add(c);
-                }
-                if (!aminoAcids.isEmpty()) aminoAcidPattern.addPTMSite(siteIndex, aminoAcids);
-                ++siteIndex;
-                numAAs += matcher.group(1).length();
-                ++numSites;
-            }
-            if (numAAs + numSites * 2 != aminoAcidPatternAsString.length()){
-                throw new IllegalArgumentException("Not all parts are in between square brackets.");
-            }
-            */
+        
         }
+        
         return aminoAcidPattern;
-        
-        
-        
-        
-        
-        /*
-        
-        for (int i = 0; i < aminoAcidPattenrAsStringCharArray.length; i++) {
-            char character = aminoAcidPattenrAsStringCharArray[i];
-            ArrayList<Character> aminoAcids = new ArrayList<Character>(1);
-            if (character == '[') {
-                if (i + 1 == aminoAcidPattenrAsStringCharArray.length) {
-                    throw new IllegalArgumentException("Reached the end of the amino acid pattern while parsing amino acid combination in " + aminoAcidPatternAsString + ".");
-                }
-                while ((character = aminoAcidPattenrAsStringCharArray[++i]) != ']') {
-                    AminoAcid.getAminoAcid(character);
-                    aminoAcids.add(character);
-                    if (i + 1 == aminoAcidPattenrAsStringCharArray.length) {
-                        throw new IllegalArgumentException("Reached the end of the amino acid pattern while parsing amino acid combination in " + aminoAcidPatternAsString + ".");
-                    }
-                }
-            } else {
-                AminoAcid.getAminoAcid(character);
-                aminoAcids.add(character);
-            }
-            aminoAcidPattern.setTargeted(index, aminoAcids);
-            index++;
-        }
-        return aminoAcidPattern;
-        */
     }
         
 

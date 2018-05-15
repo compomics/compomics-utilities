97fcaf1ab7b077d590108f0cb9e1ec5711f86e0f
mvaudel <mvaudel@gmail.com>
2018-05-15 15:10:40 -0400

    Clean-up in the spectrum counting parameters.

---
+++ src/main/java/com/compomics/util/experiment/quantification/spectrumcounting/SpectrumCountingMethod.java
@@ -0,0 +1,12 @@
+package com.compomics.util.experiment.quantification.spectrumcounting;
+
+/**
+ * Enum of the implemented spectrum counting methods.
+ *
+ * @author Marc Vaudel
+ */
+public enum SpectrumCountingMethod {
+    
+        EMPAI, NSAF;
+
+}

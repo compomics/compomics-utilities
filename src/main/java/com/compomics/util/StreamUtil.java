88034de21419a09fa9362c25a36f4c7409387c74
Harald Barsnes <harald.barsnes@gmail.com>
2020-03-03 19:21:12 +0100

    Minor JavaDoc fixes.

--- src/main/java/com/compomics/util/StreamUtil.java
+++ src/main/java/com/compomics/util/StreamUtil.java
@@ -9,29 +9,32 @@ import java.util.stream.Stream;
  * @author Marc Vaudel
  */
 public class StreamUtil {
-    
+
     /**
-     * Returns a concatenation of the streams of the given collections. If a collection is null it is ignored. Returns null if all collections are null.
-     * 
-     * @param collections The collections.
-     * 
-     * @return The concatenated stream.
+     * Returns a concatenation of the streams of the given collections. If a
+     * collection is null it is ignored. Returns null if all collections are
+     * null.
+     *
+     * @param <T> the collections
+     * @param collections the collections
+     *
+     * @return the concatenated stream
      */
-    public static <T> Stream<T>  concatenate(Collection<T>... collections) {
-        
+    public static <T> Stream<T> concatenate(Collection<T>... collections) {
+
         Stream<T> result = null;
-        
+
         for (Collection<T> collection : collections) {
-            
+
             if (collection != null) {
-                
+
                 result = result == null ? collection.stream() : Stream.concat(result, collection.stream());
-                
+
             }
         }
-        
+
         return result;
-        
+
     }
 
 }

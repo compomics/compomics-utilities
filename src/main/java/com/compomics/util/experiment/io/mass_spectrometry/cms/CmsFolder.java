787a013c0934f4c6f7fd4ec1c1bc5d6681130087
Harald Barsnes <harald.barsnes@gmail.com>
2020-04-10 20:26:56 +0200

    Minor code cleanup.

--- src/main/java/com/compomics/util/experiment/io/mass_spectrometry/cms/CmsFolder.java
+++ src/main/java/com/compomics/util/experiment/io/mass_spectrometry/cms/CmsFolder.java
@@ -6,7 +6,7 @@ package com.compomics.util.experiment.io.mass_spectrometry.cms;
  * @author Marc Vaudel
  */
 public class CmsFolder {
-    
+
     /**
      * The folder to use when creating cms files.
      */
@@ -15,26 +15,28 @@ public class CmsFolder {
      * The sub folder where the cms files should be stored.
      */
     private static final String SUB_FOLDER = ".cms_temp";
+
     /**
      * Returns the parent folder where to write cms files. Null if not set.
-     * 
+     *
      * @return The parent folder where to write cms files.
      */
     public static String getParentFolder() {
         return parentFolder;
     }
+
     /**
      * Returns the sub-folder where to write cms files. Null if not set.
-     * 
+     *
      * @return the sub-folder where to write cms files
      */
     public static String getSubFolder() {
         return SUB_FOLDER;
     }
-    
+
     /**
      * Sets the parent folder where to write cms files to.
-     * 
+     *
      * @param newParentFolder The parent folder where to write cms files to.
      */
     public static void setParentFolder(

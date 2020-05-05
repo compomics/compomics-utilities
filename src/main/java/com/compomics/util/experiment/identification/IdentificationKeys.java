7ffc1ac73b09cbbeeb07e2075a774ca07090cee3
Harald Barsnes <harald.barsnes@gmail.com>
2020-05-05 15:51:19 +0200

    Updated zoodb to version 0.6.0.

--- src/main/java/com/compomics/util/experiment/identification/IdentificationKeys.java
+++ src/main/java/com/compomics/util/experiment/identification/IdentificationKeys.java
@@ -27,7 +27,7 @@ public class IdentificationKeys extends ExperimentObject {
     /**
      * Map mapping spectra per file.
      */
-    public HashMap<String, HashSet<Long>> spectrumIdentification = new HashMap<>(1);
+    public HashMap<String, HashSet<Long>> spectrumIdentification = new HashMap<>();
     /**
      * A map linking protein accessions to all their protein matches keys.
      */

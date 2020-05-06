7e13726b6b895f8813dbdf364feebd8e74fba389
Harald Barsnes <harald.barsnes@gmail.com>
2020-05-06 11:03:53 +0200

    Added a toString method for the SpectrumCountingMethod enum.

--- src/main/java/com/compomics/util/experiment/quantification/spectrumcounting/SpectrumCountingMethod.java
+++ src/main/java/com/compomics/util/experiment/quantification/spectrumcounting/SpectrumCountingMethod.java
@@ -4,6 +4,7 @@ package com.compomics.util.experiment.quantification.spectrumcounting;
  * Enum of the implemented spectrum counting methods.
  *
  * @author Marc Vaudel
+ * @author Harald Barsnes
  */
 public enum SpectrumCountingMethod {
     
@@ -11,8 +12,23 @@ public enum SpectrumCountingMethod {
 
 
     /**
-     * Empty default constructor
+     * Empty default constructor.
      */
     private SpectrumCountingMethod() {
+    }
+    
+    @Override
+    public String toString() {
+
+        switch (this) {
+            case EMPAI:
+                return "emPAI";
+            case NSAF:
+                return "NSAF+";
+            default:
+                throw new UnsupportedOperationException(
+                        "Spectrum counting method " + this.name() + " not implemented.");
+        }
+
     }
 }

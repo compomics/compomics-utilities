dfac0383aea82f114c9e45aa58a036b2c70dfe07
Harald Barsnes <harald.barsnes@gmail.com>
2020-06-24 16:26:29 +0200

    Implemented our own mzML parser (given that jmzML does not work on recent Java versions). Added progress display to the X! Tandem parser. Made the JavaDoc compatible with HTML5 (and thus newer Java JDKs).

--- src/main/java/com/compomics/util/protein/ModificationImplementation.java
+++ src/main/java/com/compomics/util/protein/ModificationImplementation.java
@@ -217,24 +217,24 @@ public class ModificationImplementation implements com.compomics.util.interfaces
      * The general contract of <code>hashCode</code> is:
      * <ul>
      * <li>Whenever it is invoked on the same object more than once during
-     *     an execution of a Java application, the <tt>hashCode</tt> method
+     *     an execution of a Java application, the <code>hashCode</code> method
      *     must consistently return the same integer, provided no information
-     *     used in <tt>equals</tt> comparisons on the object is modified.
+     *     used in <code>equals</code> comparisons on the object is modified.
      *     This integer need not remain consistent from one execution of an
      *     application to another execution of the same application.
-     * <li>If two objects are equal according to the <tt>equals(Object)</tt>
+     * <li>If two objects are equal according to the <code>equals(Object)</code>
      *     method, then calling the <code>hashCode</code> method on each of
      *     the two objects must produce the same integer result.
      * <li>It is <em>not</em> required that if two objects are unequal
      *     according to the {@link Object#equals(Object)}
-     *     method, then calling the <tt>hashCode</tt> method on each of the
+     *     method, then calling the <code>hashCode</code> method on each of the
      *     two objects must produce distinct integer results.  However, the
      *     programmer should be aware that producing distinct integer results
      *     for unequal objects may improve the performance of hashtables.
      * </ul>
      * <p>
      * As much as is reasonably practical, the hashCode method defined by
-     * class <tt>Object</tt> does return distinct integers for distinct
+     * class <code>Object</code> does return distinct integers for distinct
      * objects. (This is typically implemented by converting the internal
      * address of the object into an integer, but this implementation
      * technique is not required by the Java programming language.)

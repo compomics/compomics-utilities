8c35228c135e14bc04bf91c37f1ec6fdb73c562a
Harald Barsnes <harald.barsnes@gmail.com>
2021-09-07 14:34:46 +0200

    Minor code cleanup.

--- src/main/java/com/compomics/util/experiment/identification/protein_inference/fm_index/AccessionMetaData.java
+++ src/main/java/com/compomics/util/experiment/identification/protein_inference/fm_index/AccessionMetaData.java
@@ -2,7 +2,6 @@ package com.compomics.util.experiment.identification.protein_inference.fm_index;
 
 import com.compomics.util.TempByteArray;
 import com.compomics.util.io.compression.ZstdUtils;
-import io.airlift.compress.zstd.ZstdCompressor;
 import java.io.Serializable;
 import java.util.Arrays;
 
@@ -56,22 +55,27 @@ public class AccessionMetaData implements Serializable {
      * @return the parsed header
      */
     public String getHeaderAsString() {
-        
+
         byte[] decompressedHeader = ZstdUtils.zstdDecompress(headerAsCompressedString, uncompressedLength);
-        
+
         return new String(decompressedHeader);
-    
+
     }
-    
+
+    /**
+     * Set the header as string.
+     * 
+     * @param header the header
+     */
     public void setHeaderAsString(
-    String header
+            String header
     ) {
-        
+
         byte[] headerBytes = header.getBytes();
         this.uncompressedLength = headerBytes.length;
-        
+
         TempByteArray tempByteArray = ZstdUtils.zstdCompress(headerBytes);
         headerAsCompressedString = Arrays.copyOf(tempByteArray.array, tempByteArray.length);
-        
+
     }
 }

62c9046e79011c35c4e0af59eb54da0a82847c98
mvaudel <mvaudel@gmail.com>
2022-03-01 21:39:04 +0100

    Clean-up

--- src/main/java/com/compomics/util/experiment/identification/modification/peptide_mapping/ModificationPeptideMapping.java
+++ src/main/java/com/compomics/util/experiment/identification/modification/peptide_mapping/ModificationPeptideMapping.java
@@ -3,7 +3,6 @@ package com.compomics.util.experiment.identification.modification.peptide_mappin
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 import org.jgrapht.Graph;
@@ -20,11 +19,13 @@ import org.jgrapht.alg.interfaces.MatchingAlgorithm.Matching;
 public class ModificationPeptideMapping {
 
     /**
-     * Returns map of site to modification matchings.
+     * Returns map of site to modification mapping.
      * 
      * @param modificationToPossibleSiteMap Map of modification mass to site to modification names.
      * @param modificationOccurrenceMap Map of modification mass to number of modifications.
      * @param modificationToSiteToScore Map of modification mass to modification site to localization score.
+     * 
+     * @return The site to modification mass mapping.
      */
     public static HashMap<Integer, Double> mapModifications(
             //HashMap<Double, HashMap<Integer, ArrayList<String>>> modificationToPossibleSiteMap,
@@ -35,40 +36,48 @@ public class ModificationPeptideMapping {
 
         Graph<String, DefaultWeightedEdge> g = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
         
-        Set<String> sitesVertices = new HashSet<String>();
-        Set<String> modificationsVertices = new HashSet<String>();
+        Set<String> sitesVertices = new HashSet<>();
+        Set<String> modificationsVertices = new HashSet<>();
 
-        Iterator iterator = modificationToPossibleSiteMap.entrySet().iterator();
-        
-        while (iterator.hasNext()) {
+        for (Map.Entry<Double, ArrayList<Integer>> modificationEntry : modificationToPossibleSiteMap.entrySet()) {
             
-            Map.Entry modificationEntry = (Map.Entry)iterator.next();
-            Double modID = (Double) modificationEntry.getKey();
-            Integer modOccNum = modificationOccurrenceMap.get(modID);
-            for (int i=0; i<modOccNum; i++){
+            double modID = modificationEntry.getKey();
+            int modOccNum = modificationOccurrenceMap.get(modID);
+            
+            for (int i = 0; i < modOccNum; i++){
+            
                 String vertexName = String.join("-",String.valueOf(modID),String.valueOf(i));
                 g.addVertex(vertexName);
                 modificationsVertices.add(vertexName);
+                
             }
             
             HashMap<Integer, Double> localizationScores = modificationToSiteToScore.get(modID);
             
-            ArrayList<Integer> sites = (ArrayList<Integer>) modificationEntry.getValue();
-            for (Integer site : sites){
+            ArrayList<Integer> sites = modificationEntry.getValue();
+            
+            for (int site : sites) {
+                
                 String siteVertexName = String.valueOf(site);
+                
                 if (!g.vertexSet().contains(siteVertexName)){
+                    
                     g.addVertex(siteVertexName);
                     sitesVertices.add(siteVertexName);
+                    
                 }
-                Double locScore = localizationScores.get(site);
-                for (int i=0; i<modOccNum; i++){
-                    String modVertexName = String.join("-",String.valueOf(modID),String.valueOf(i));
-                    DefaultWeightedEdge e = g.addEdge(modVertexName, siteVertexName); 
+                
+                double locScore = localizationScores.get(site);
+                
+                for (int i = 0; i < modOccNum; i++){
+                    
+                    String modVertexName = String.join("-", String.valueOf(modID), String.valueOf(i));
+                    DefaultWeightedEdge e = g.addEdge(modVertexName, siteVertexName);
                     g.setEdgeWeight(e, locScore); 
+                    
                 }
                 
             }
-            
         }
         
         MaximumWeightBipartiteMatching​ matching = new MaximumWeightBipartiteMatching​(g, modificationsVertices, sitesVertices);
@@ -78,12 +87,15 @@ public class ModificationPeptideMapping {
         Set<DefaultWeightedEdge> matchingEdges = matchingInfo.getEdges();
         
         HashMap<Integer, Double> matchedSiteToModification = new HashMap<>(1);
+        
         for(DefaultWeightedEdge e : matchingEdges){
+            
             String eInfo = e.toString();
             String[] eVertices = eInfo.split(":");
-            Double modVertex = Double.parseDouble(eVertices[0].substring(1,eVertices[0].length()-1).split("-")[0]);
-            Integer siteVertex = Integer.parseInt(eVertices[1].substring(1,eVertices[1].length()-1));
+            double modVertex = Double.parseDouble(eVertices[0].substring(1,eVertices[0].length()-1).split("-")[0]);
+            int siteVertex = Integer.parseInt(eVertices[1].substring(1,eVertices[1].length()-1));
             matchedSiteToModification.put(siteVertex, modVertex);
+            
         }
         
         //System.out.println(matchedSiteToModification);

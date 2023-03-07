90b940d716f48f73fe981c38764f021e1f87fb30
Harald Barsnes <harald.barsnes@gmail.com>
2023-03-07 11:01:50 +0100

    Added a precursor map in the cms files. Moved project to version 5.0.66.

---
+++ src/main/java/com/compomics/util/experiment/mass_spectrometry/spectra/PrecursorParameter.java
@@ -0,0 +1,65 @@
+package com.compomics.util.experiment.mass_spectrometry.spectra;
+
+import com.compomics.util.experiment.personalization.UrParameter;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * Parameter for storing additional non-mandatory precursor information.
+ *
+ * @author Harald Barsnes
+ */
+public class PrecursorParameter implements UrParameter, Serializable {
+
+    /**
+     * Serial version UID for post-serialization compatibility.
+     */
+    static final long serialVersionUID = -2767016431482105597L;
+    /**
+     * The precusor identifier.
+     */
+    private ArrayList<String> precusorIdentifiers;
+    /**
+     * An empty parameter used for instantiation.
+     */
+    public static final PrecursorParameter dummy = new PrecursorParameter();
+
+    /**
+     * Constructor.
+     */
+    public PrecursorParameter() {
+    }
+    
+    /**
+     * Constructor.
+     *
+     * @param precusorIdentifiers the precursor identifiers
+     */
+    public PrecursorParameter(ArrayList<String> precusorIdentifiers) {
+        this.precusorIdentifiers = precusorIdentifiers;
+    }
+
+    @Override
+    public long getParameterKey() {
+        return serialVersionUID;
+    }
+
+    /**
+     * Returns the precusor identifiers.
+     *
+     * @return the precusorIdentifiers
+     */
+    public ArrayList<String> getPrecusorIdentifiers() {
+        return precusorIdentifiers;
+    }
+
+    /**
+     * Set the precursor identifier.
+     *
+     * @param precusorIdentifiers the precusorIdentifiers to set
+     */
+    public void setPrecusorIdentifiers(ArrayList<String> precusorIdentifiers) {
+        this.precusorIdentifiers = precusorIdentifiers;
+    }
+
+}
--- src/main/java/com/compomics/util/experiment/personalization/UrParameter.java
+++ src/main/java/com/compomics/util/experiment/personalization/UrParameter.java
@@ -1,16 +1,16 @@
 package com.compomics.util.experiment.personalization;
 
-
 /**
  * This interface is used to reference refinement parameters.
- * 
+ *
  * @author Marc Vaudel
  */
 public interface UrParameter {
 
     /**
-     * This method returns the key of the parameter. The key must be unique to the type of parameter.
-     * 
+     * This method returns the key of the parameter. The key must be unique to
+     * the type of parameter.
+     *
      * @return the parameter key
      */
     public long getParameterKey();
